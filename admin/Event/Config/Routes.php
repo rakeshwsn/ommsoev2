d83ff5489272b537282c39461b7bbc6a97a88b55
rakeshwsn <rakesh@wassan.org>
2023-02-27 18:25:57 +0530

    Event module added

---
+++ admin/Event/Config/Routes.php
@@ -0,0 +1,17 @@
+<?php
+namespace Admin\Event\Config;
+if(!isset($routes))
+{
+    $routes = \Config\Services::routes(true);
+}
+
+$routes->group(env('app.adminRoute'), ['namespace' => 'Admin','filter' => 'login'], function($routes)
+{
+    $routes->add('event', 'Event\Controllers\Event::index');
+    $routes->post('event/search','Event\Controllers\Event::search');
+    $routes->match(['get','post'],'event/add', 'Event\Controllers\Event::add');
+    $routes->match(['get','post'],'event/edit/(:segment)', 'Event\Controllers\Event::edit/$1');
+    $routes->get('event/delete/(:segment)',   'Event\Controllers\Event::delete/$1');
+    $routes->post('event/delete','Event\Controllers\Event::delete');
+
+});
