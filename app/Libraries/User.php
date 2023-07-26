16d30b97692959e0d0ab37d29861e622657561e3
niranjan-wassan <niranjan@wassan.org>
2023-07-26 12:21:31 +0530

    permission fix

--- app/Libraries/User.php
+++ app/Libraries/User.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Libraries;
 
-use Admin\Permission\Models\PermissionModel;
+use Admin\Permissions\Models\PermissionsModel;
 use Admin\Users\Models\UserGroupModel;
 use Admin\Users\Models\UserModel;
 
@@ -55,10 +55,12 @@ class User
 		$this->appuser_token	= $user->central_appuser_token;
 		$permissions = json_decode($user_group->permissions, true);
 
-        $permissionModel = new PermissionModel();
-        $user_permission=$permissionModel->get_modules_with_permission($this->user_group_id);
+        $permissionModel = new PermissionsModel();
+        $user_permission=$permissionModel->get_modules_with_permissions($this->user_group_id);
+       
         foreach ( $user_permission as $value ) {
-            $name = str_replace('_', '/', $value->name);
+            //$name = str_replace('_', '/', $value->name);
+            $name=$value->route;
             $this->permission[$name] = $value->active;
         }
 
@@ -127,7 +129,7 @@ class User
     }
 
     public function hasPermission($data) {
-
+        //echo $data;
         $subUrl = ['add','edit','view','delete','download'];
         $other_permission=false;
         foreach ($subUrl as $value) {
@@ -142,9 +144,9 @@ class User
             $other_permission=false;
         }
         
-       // printr($this->permission);
-       // echo $data;
-       // exit;
+        //print_r($this->permission);
+        //echo $data."<br>";
+        //exit;
         if ($this->user_group_id == 1) {
             return true;
         }else if(isset($this->permission[$data]) && $this->permission[$data] == 'yes') {
@@ -152,7 +154,7 @@ class User
         }else if(isset($this->permission[$data]) && $this->permission[$data] == 'no'){
             return false;
         }else if($other_permission){
-            return true;
+            //return true;
         }
         return false;
 
