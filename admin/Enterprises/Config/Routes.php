ac206a2b3dc55e6d530cb6a20721e8d21e13a46b
saraswatee2000 <saraswateebhoi143@gmail.com>
2023-08-04 19:30:20 +0530

    changed

--- admin/Enterprises/Config/Routes.php
+++ admin/Enterprises/Config/Routes.php
@@ -15,22 +15,6 @@ $routes->group('admin', ['namespace' => 'Admin', 'filter' => 'login'], function 
     // enterprises
     $routes->add('enterprises/add', 'Enterprises\Controllers\Enterprises::add');
     $routes->add('enterprises', 'Enterprises\Controllers\Enterprises::index');
-<<<<<<< HEAD
-    $routes->add('enterprises/blocks', 'Enterprises\Controllers\Enterprises::ajaxblocks');
-    $routes->add('enterprises/gps', 'Enterprises\Controllers\Enterprises::ajaxgps');
-    $routes->add('enterprises/villages', 'Enterprises\Controllers\Enterprises::ajaxvillages');
-    $routes->add('enterprises/edit', 'Enterprises\Controllers\Enterprises::edit');
-
-    // $routes->add('enterprises', 'Enterprises\Controllers\Enterprises::enterprises');
-    // $routes->get('ajax/blocks', 'Enterprises\Controllers\Enterprises::ajaxBlocks');
-
-//Budget
-$routes->add('budgetcode', 'Enterprises\Controllers\Budget::index');
-$routes->add('budgetcode/edit/(:num)', 'Enterprises\Controllers\Budget::edit/$1');
-$routes->add('budgetcode/add', 'Enterprises\Controllers\Budget::add');
-$routes->add('budgetcode/search', 'Enterprises\Controllers\Budget::search');
-$routes->add('budgetcode/delete/(:num)', 'Enterprises\Controllers\Budget::delete/$1');
-=======
     $routes->post('enterprises/search', 'Enterprises\Controllers\Enterprises::search',['permission'=>false]);
     // $routes->post('incentive/searchmain', 'Incentive\Controllers\Incentive::searchMain');
     // $routes->match(['get', 'post'], 'incentive/view/(:segment)', 'Incentive\Controllers\Incentive::view/$1');
@@ -41,5 +25,4 @@ $routes->add('budgetcode/delete/(:num)', 'Enterprises\Controllers\Budget::delete
     // $routes->post('incentive/delete', 'Incentive\Controllers\Incentive::delete');
     // $routes->add('incentive/incentivesearch', 'Incentive\Controllers\Incentive::incentivesearch');
     // $routes->post('incentive/searchall', 'Incentive\Controllers\Incentive::searchall');
->>>>>>> 526a4ae32e32441203cb5f267041a2fac3a63edd
 });
