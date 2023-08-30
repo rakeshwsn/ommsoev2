8ea75d3d89e9b8c9908862171985ff1465418020
hemantaWASSAN <132560469+hemantaWASSAN@users.noreply.github.com>
2023-08-30 19:54:24 +0530

    adding week wise filter

--- admin/CropCoverage/Config/Routes.php
+++ admin/CropCoverage/Config/Routes.php
@@ -9,6 +9,8 @@ $routes->group('admin', ['namespace' => 'Admin', 'filter' => 'login'], function 
     $routes->match(['get', 'post'], 'cropcoverage/crops', 'CropCoverage\Controllers\AreaCoverageCrops::index');
     $routes->add('areacoverage/download', 'CropCoverage\Controllers\AreaCoverage::download');
     $routes->match(['get', 'post'], 'areacoverage', 'CropCoverage\Controllers\AreaCoverage::Index');
+    $routes->match(['get', 'post'], 'areacoverage/filtered', 'CropCoverage\Controllers\AreaCoverage::weekWiseFilterdData');
+
     $routes->post('areacoverage/search', 'CropCoverage\Controllers\AreaCoverage::search', ['permission' => false]);
     $routes->match(['get', 'post'], 'areacoverage/target', 'CropCoverage\Controllers\AreaCoverageTarget::index');
     $routes->match(['get', 'post'], 'cropcoverage/reports', 'CropCoverage\Controllers\CropCoverageReports::index');
@@ -25,7 +27,7 @@ $routes->group('admin', ['namespace' => 'Admin', 'filter' => 'login'], function 
     $routes->get('areacoverage/dashboard/chart', 'CropCoverage\Controllers\AreaCoverageDashboard::chart');
     $routes->get('areacoverage/dashboard/milletchart', 'CropCoverage\Controllers\AreaCoverageDashboard::milletChart');
     $routes->add('areacoverage/approve/district', 'CropCoverage\Controllers\Approve::district');
-//    $routes->post('areacoverage/approve/reject', 'CropCoverage\Controllers\Approve::reject');
+    //    $routes->post('areacoverage/approve/reject', 'CropCoverage\Controllers\Approve::reject');
     $routes->get('dashboard/chart', 'CropCoverage\Controllers\AreaCoverageDashboard::chart');
 
 
