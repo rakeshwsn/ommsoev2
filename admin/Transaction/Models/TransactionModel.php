1b5f826e0cac3e3a4205f423d5d368a897a1ded5
Rakesh <rakesh@wassan.org>
2023-10-31 15:54:10 +0530

    testing push

--- admin/Transaction/Models/TransactionModel.php
+++ admin/Transaction/Models/TransactionModel.php
@@ -133,6 +133,11 @@ AND tc.deleted_at IS NULL";
         return $this->db->query($sql)->getResult();
     }
 
+    //test push
+    public function test() {
+        //tthis this test function
+    }
+
     public function getTotal($filter=[]) {
 
         $sql = "SELECT
