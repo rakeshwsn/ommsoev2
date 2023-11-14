cc897fcd15bc99ba80ad24c8c0dd410f14a95574
saraswatee2000 <saraswateebhoi143@gmail.com>
2023-11-14 17:44:38 +0530

    changes on establishment transaction

--- admin/Enterprises/Models/EstablishmentTransactionDetailsModel.php
+++ admin/Enterprises/Models/EstablishmentTransactionDetailsModel.php
@@ -44,66 +44,83 @@ class EstablishmentTransactionDetailsModel extends Model
   public function periodswisetrans($filter = [])
   {
     $sql = "SELECT
-    et1.id,
-    et.unit_id,
-    et.period,
-    et.district_id,
-    et.year_id,
-    et.month_id,
-    et1.transaction_id,
-    et1.block_id,
-    et1.gp_id,
-    et.created_at,
-    et1.village_id,
-    et1.no_of_days_functional,
-    et1.produced,
-    et1.charges_per_qtl,
-    et1.total_expend,
-    et1.total_turnover,
-    sm.name month_name,
-    dy.name year_name,
+    txn_dtl.txn_id,
+    txn_dtl.id,
     sd.name district_name,
     sb.name block_name,
-    g.name gp_name,
     v.name village_name,
+    g.name gp_name,
+    txn_dtl.no_of_days_functional,
+    txn_dtl.charges_per_qtl,
+    txn_dtl.total_expend,
+    txn_dtl.enterprise_id,
+    txn_dtl.district_id,
+    txn_dtl.block_id,
+    txn_dtl.village_id,
+    txn_dtl.gp_id,
+    txn_dtl.produced,
+    txn_dtl.total_turnover,
+    dy.name year_name,
+    sm.name month_name,
     eu.name unit_name,
-    CASE WHEN et.period = 1 THEN '1st fortnight' ELSE '2nd fortnight' END  period_type
-    FROM enterprises_transaction et
-    LEFT JOIN enterprises_transactiondetails et1
-      ON et.id = et1.transaction_id
-    LEFT JOIN soe_months sm
-      ON sm.id = et.month_id
-    LEFT JOIN dashboard_years dy
-      ON dy.id = et.year_id
-    LEFT JOIN soe_districts sd
-      ON sd.id = et.district_id
+    txn_dtl.period period_type,txn_dtl.created_at
+  FROM (SELECT
+      etd.id id,
+      etd.no_of_days_functional,
+      etd.produced,
+      etd.total_turnover,
+      etd.charges_per_qtl,
+      etd.total_expend,
+      et.id txn_id,
+      etd.enterprise_id,
+      et.district_id,
+      et.period,
+      etd.block_id,
+      etd.gp_id,
+      etd.village_id,
+      et.created_at,
+      et.year_id,
+      et.month_id,
+      et.unit_id
+    FROM enterprises_transactiondetails etd
+      INNER JOIN enterprises_transaction et
+        ON etd.transaction_id = et.id
+    WHERE et.deleted_at IS NULL
+    AND etd.deleted_at IS NULL) txn_dtl
+    LEFT JOIN enterprises e
+      ON e.id = txn_dtl.enterprise_id
     LEFT JOIN soe_blocks sb
-      ON sb.id = et1.block_id
+      ON sb.id = txn_dtl.block_id
+    LEFT JOIN soe_districts sd
+      ON sd.id = txn_dtl.district_id
     LEFT JOIN grampanchayat g
-      ON g.id = et1.gp_id
+      ON g.id = txn_dtl.gp_id
     LEFT JOIN villages v
-      ON v.id = et1.village_id
+      ON v.id = txn_dtl.village_id
+    LEFT JOIN dashboard_years dy
+      ON dy.id = txn_dtl.year_id
+    LEFT JOIN soe_months sm
+      ON sm.id = txn_dtl.month_id
     LEFT JOIN enterprises_units eu
-      ON eu.id = et.unit_id
-  WHERE et1.deleted_at IS NULL ";
-  if (isset($filter['id'])) {
-    $sql .= " AND et1.id = " . $filter['id'];
-  }
+      ON eu.id = txn_dtl.unit_id WHERE 1=1";
+    if (isset($filter['id'])) {
+      $sql .= " AND txn_dtl.id = " . $filter['id'];
+    }
     if (isset($filter['year_id'])) {
-      $sql .= " AND et.year_id = " . $filter['year_id'];
+      $sql .= " AND txn_dtl.year_id = " . $filter['year_id'];
     }
     if (isset($filter['district_id'])) {
-      $sql .= " AND et.district_id = " . $filter['district_id'];
+      $sql .= " AND txn_dtl.district_id = " . $filter['district_id'];
     }
     if (isset($filter['month_id'])) {
-      $sql .= " AND et.month_id = " . $filter['month_id'];
+      $sql .= " AND txn_dtl.month_id = " . $filter['month_id'];
     }
-    if (isset($filter['period'])) {
-      $sql .= " AND et.period = " . $filter['period'];
+    if (isset($filter['period_type'])) {
+      $sql .= " AND txn_dtl.period_type = " . $filter['period_type'];
     }
     // $sql .=  " GROUP BY unit.units";
-    // echo $sql;exit;
+    //  echo $sql;exit;
     return $this->db->query($sql)->getResult();
   }
- 
+  
 }
