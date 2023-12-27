d1bbc27500143d4b779b553cc3274e6e18aefa3f
hemantaWASSAN <132560469+hemantaWASSAN@users.noreply.github.com>
2023-12-27 16:47:41 +0530

    finaldata doc upload module

---
+++ admin/CropCoverage/Models/FinalDataDocModel.php
@@ -0,0 +1,59 @@
+<?php
+namespace Admin\CropCoverage\Models;
+
+use CodeIgniter\Model;
+
+class FinalDataDocModel extends Model
+{
+    protected $DBGroup = 'default';
+    protected $table = 'ac_finaldata_doc';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID = 0;
+    protected $returnType = 'object';
+    protected $useSoftDeletes = true;
+    protected $protectFields = false;
+    // protected $allowedFields = [
+    // 	'block_id',
+    // 	'year_id',
+    // 	'season',
+    // 	'ragi_smi',
+    // 	'ragi_lt',
+    // 	'ragi_ls',
+    // ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+
+    // Validation
+    protected $validationRules = [
+
+    ];
+
+
+    protected $validationMessages = [];
+    protected $skipValidation = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
+
+    public function addFileData($filedata)
+    {
+        $this->insert($filedata);
+    }
+
+}
+?>
