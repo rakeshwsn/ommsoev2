4f47f884a7d70bdae80781176d518a142aab445a
rakeshwassan <rnayak@wassan.org>
2024-01-03 11:11:15 +0530

    incentive status pdf view

--- admin/Common/Views/header.php
+++ admin/Common/Views/header.php
@@ -27,7 +27,9 @@ $user = service('user');
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,400i,600,700&display=swap">
     <link rel="stylesheet" id="css-main" href="<?= theme_url('assets/css/codebase.min.css'); ?>">
     <link rel="stylesheet" id="css-main" href="<?= theme_url('assets/css/custom.css?v=3'); ?>">
-     
+    <link rel="stylesheet" id="css-main" href="<?= theme_url('assets/dflip/css/dflip.css?v=1'); ?>">
+    <link rel="stylesheet" id="css-main" href="<?= theme_url('assets/dflip/css/themify-icons.css'); ?>">
+
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/modules/variwide.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
@@ -39,7 +41,7 @@ $user = service('user');
     <!-- Controller Defined Stylesheets -->
     <?= $template->stylesheets() ?>
     <script src="<?= theme_url('assets/js/codebase.core.min.js'); ?>"></script>
-    
+
 
     <script type="text/javascript">
         var BASE_URL = '<?= base_url(); ?>';
