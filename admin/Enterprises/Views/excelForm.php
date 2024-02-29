0de49d1dfc04c1448877a84bcee06b87fb5886cb
saraswatee2000 <saraswateebhoi143@gmail.com>
2024-02-29 15:18:03 +0530

    enterprise txn bug incomplete

--- admin/Enterprises/Views/excelForm.php
+++ admin/Enterprises/Views/excelForm.php
@@ -19,7 +19,6 @@
             </div>
         </td>
     </tr>
-    
     <?php foreach ($enterprises as $enterprise) { ?>
         <?php $slno = 1; ?>
         <thead>
@@ -32,18 +31,20 @@
                 <td><?= $enterprise->unit_id ?></td>
                 <th style="text-align: center; background-color:#848403 ;border: 5px solid black; font-weight: bold;">ent_id</th>
                 <th style="text-align: center; background-color:#848403;border: 5px solid black; font-weight: bold; ">Slno</th>
-                <th contenteditable="false" style="text-align: center; background-color:#848403;border: 5px solid black; font-weight: bold; ">Block</th>
+                <th style="text-align: center; background-color:#848403;border: 5px solid black; font-weight: bold; ">Block</th>
                 <th>block_id</th>
-                <th contenteditable="false" style="text-align: center; background-color:#848403;border: 5px solid black; font-weight: bold; ">GP</th>
+                <th style="text-align: center; background-color:#848403;border: 5px solid black; font-weight: bold; ">GP</th>
                 <th>gp_id</th>
-                <th contenteditable="false" style="text-align: center; background-color:#848403;border: 5px solid black; font-weight: bold; ">Village</th>
+                <th style="text-align: center; background-color:#848403;border: 5px solid black; font-weight: bold; ">Village</th>
                 <th>village_id</th>
-                <th contenteditable="false" style="text-align: center; background-color:#848403;border: 5px solid black; font-weight: bold; ">Name SHG </th>
+                <th style="text-align: center; background-color:#848403;border: 5px solid black; font-weight: bold; ">Name SHG </th>
                 <?php foreach ($columns as $key => $column) { ?>
                     <th style="text-align: center; background-color:#848403;border: 5px solid black; font-weight: bold; "><?= $column['label'] ?></th>
                 <?php } ?>
             </tr>
         </thead>
+
+
         <tbody>
             <?php foreach ($enterprises as $enterprise) { ?>
                 <tr>
