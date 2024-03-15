ac76fb0170f3ca600466671c61f3202d9f023d92
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:17 +0530

    improve

--- app/Commands/ControllerCreate.php
+++ app/Commands/ControllerCreate.php
@@ -40,43 +40,55 @@ class ControllerCreate extends BaseCommand
      *
      * @var array
      */
-    protected $arguments = ['ControllerName' => 'Controller name to be created'];
+    protected $arguments = [
+        'ControllerName' => 'Controller name to be created'
+    ];
 
     /**
      * The Command's Options
      *
      * @var array
      */
-    protected $options      = [
+    protected $options = [
         '-f' => 'Set module folder inside app path (default Modules)',
         '-v' => 'Set view folder inside app path (default front/module/Views)',
     ];
-	
-	 /**
-     * Parent Controller Name 
+
+    /**
+     * Parent Controller Name
+     * @var string
      */
-	
-	protected $parent_controller;
+    protected $parent_controller = 'Controller';
 
     /**
      * Module Name to be Created
+     * @var string
      */
     protected $module_name;
 
-
     /**
      * Module folder (default /Modules)
+     * @var string
      */
-    protected $module_folder;
+    protected $module_folder = 'Modules';
 
+    /**
+     * View folder (default /front/module/Views)
+     * @var string
+     */
+    protected $view_folder = 'front/module/Views';
 
-	/**
+    /**
      * Actually execute a command.
      *
      * @param array $params
      */
     public function run(array $params)
     {
-       //
-    }
-}
+        // Validate the ControllerName argument
+        if (empty($params['ControllerName'])) {
+            CLI::write('Error: Controller name is required.', 'red');
+            exit(1);
+        }
+
+        $this->module_name = ucwords(strtolower($
