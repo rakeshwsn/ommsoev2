c2b5a09e41d6e48a36309f5cde1811ebb758f729
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:11 +0530

    improve

--- admin/Reports/Views/abstract_mpr.php
+++ admin/Reports/Views/abstract_mpr.php
@@ -1,16 +1,27 @@
 <section class="content">
+    <?php if (isset($filter_panel)): ?>
+        <?= $filter_panel; ?>
+    <?php endif; ?>
 
-    <?=$filter_panel?>
-
-    <?php if($components): ?>
-    <div class="block block-themed">
-        <div class="block-header bg-muted">
-            <h3 class="block-title">Report</h3>
-            <div class="block-options">
-                <a href="<?=$download_url?>" class="btn btn-secondary" data-toggle="tooltip" title="Download" role="button" data-original-title="Download">
-                    <i class="si si-cloud-download"></i>
-                </a>
+    <?php if (count($components) > 0): ?>
+        <div class="block block-themed">
+            <div class="block-header bg-muted">
+                <h3 class="block-title">Report</h3>
+                <div class="block-options">
+                    <?php if (isset($download_url)): ?>
+                        <a href="<?= $download_url; ?>" class="btn btn-secondary" data-toggle="tooltip" title="Download" role="button">
+                            <i class="si si-cloud-download"></i> Download
+                        </a>
+                    <?php endif; ?>
+                </div>
+            </div>
+            <div class="block-content block-content-full" style="margin-top: 1rem;">
+                <div id="tableFixHead" class="table-responsive table-fix-head">
+                    <table class="table table-bordered table-striped table-vcenter">
+                        <!-- Add table headers and rows here -->
+                    </table>
+                </div>
             </div>
         </div>
-        <div class="block-content block-content-full" style="margin-top: 1rem;">
-            <div id="tableFixHead" class="tableFixHead" style="min-width: 100%; box
+    <?php endif; ?>
+</section>
