4b1ca771f61d7ccada05a78b741edf2c5d8f536f
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:04 +0530

    improve

--- app/Config/Migrations.php
+++ app/Config/Migrations.php
@@ -7,49 +7,24 @@ use CodeIgniter\Config\BaseConfig;
 class Migrations extends BaseConfig
 {
 	/**
-	 * --------------------------------------------------------------------------
-	 * Enable/Disable Migrations
-	 * --------------------------------------------------------------------------
+	 * Whether to enable or disable migrations.
 	 *
-	 * Migrations are enabled by default.
-	 *
-	 * You should enable migrations whenever you intend to do a schema migration
-	 * and disable it back when you're done.
-	 *
-	 * @var boolean
+	 * @var bool
 	 */
-	public $enabled = true;
+	public $isEnabled = true;
 
 	/**
-	 * --------------------------------------------------------------------------
-	 * Migrations Table
-	 * --------------------------------------------------------------------------
-	 *
-	 * This is the name of the table that will store the current migrations state.
-	 * When migrations runs it will store in a database table which migration
-	 * level the system is at. It then compares the migration level in this
-	 * table to the $config['migration_version'] if they are not the same it
-	 * will migrate up. This must be set.
+	 * The name of the table that will store the current migrations state.
 	 *
 	 * @var string
 	 */
-	public $table = 'migrations';
+	public $migrationTrackingTable = 'migrations';
 
 	/**
-	 * --------------------------------------------------------------------------
-	 * Timestamp Format
-	 * --------------------------------------------------------------------------
-	 *
-	 * This is the format that will be used when creating new migrations
-	 * using the CLI command:
-	 *   > php spark migrate:create
-	 *
-	 * Typical formats:
-	 * - YmdHis_
-	 * - Y-m-d-His_
-	 * - Y_m_d_His_
+	 * The format that will be used when creating new migrations
+	 * using the CLI command.
 	 *
 	 * @var string
 	 */
-	public $timestampFormat = 'Y-m-d-His_';
+	public $timestampFormatForNewMigrations = 'Y-m-d-His_';
 }
