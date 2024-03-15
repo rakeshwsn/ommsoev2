218f3a7c03ed283c3002f07b0b64ac69a9301f69
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:04 +0530

    improve

--- admin/FPO/Config/Routes.php
+++ admin/FPO/Config/Routes.php
@@ -12,10 +12,16 @@ $routes->group(env('app.adminRoute'), [
     'namespace' => 'Admin',
     'filter' => 'login'
 ], function ($routes) {
-    $routes->add('fpo', 'FPO\Controllers\FPO::index');
-    $routes->add('fpo/create', 'FPO\Controllers\FPO::add');
-    $routes->add('fpo/edit/(:num)', 'FPO\Controllers\FPO::edit/$1');
-    $routes->add('fpo/view/(:num)', 'FPO\Controllers\FPO::details/$1');
-    $routes->add('fpo/geview/(:num)', 'FPO\Controllers\FPO::gedit/$1');
-    $routes->post('fpo/upload', 'FPO\Controllers\FPO::upload');
+    // Use route grouping to group all FPO routes
+    $fpoRoutes = function ($routes) {
+        $routes->add('', 'FPO\Controllers\FPO::index')->name('fpo.index');
+        $routes->add('create', 'FPO\Controllers\FPO::add')->name('fpo.create');
+        $routes->add('edit/(:num)', 'FPO\Controllers\FPO::edit/$1')->name('fpo.edit');
+        $routes->add('view/(:num)', 'FPO\Controllers\FPO::details/$1')->name('fpo.view');
+        $routes->add('geview/(:num)', 'FPO\Controllers\FPO::gedit/$1')->name('fpo.geview');
+        $routes->post('upload', 'FPO\Controllers\FPO::upload')->name('fpo.upload');
+    };
+
+    // Call the FPO route group
+    $fpoRoutes($routes);
 });
