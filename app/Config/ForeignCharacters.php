45c25e69903c9e368335584c80a27323f795b60d
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:13 +0530

    improve

--- app/Config/ForeignCharacters.php
+++ app/Config/ForeignCharacters.php
@@ -6,4 +6,10 @@ use CodeIgniter\Config\ForeignCharacters as BaseForeignCharacters;
 
 class ForeignCharacters extends BaseForeignCharacters
 {
+    public function convert($text)
+    {
+        // Add your custom conversion logic here
+        // For example, converting all 'a' characters to '@'
+        return str_replace('a', '@', $text);
+    }
 }
