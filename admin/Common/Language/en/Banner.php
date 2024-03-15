1c38d26dfbcfcc050f028dc62b546ace84925c7e
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:55 +0530

    improve

--- admin/Common/Language/en/Banner.php
+++ admin/Common/Language/en/Banner.php
@@ -1,10 +1,23 @@
 <?php
-// Text
-return [
-	'heading_title'        => 'Banner',
-	'text_image'           => 'Select Image',
-	'text_clear'            => 'Clear'
+
+// Define the array of text values
+$text = [
+    'heading_title'        => 'Banner',
+    'text_image'           => 'Select Image',
+    'text_clear'            => 'Clear'
 ];
 
+// Return the array of text values
+function getText() {
+    global $text;
+    return $text;
+}
 
+// Call the function to get the text values
+$textValues = getText();
+
+// Print the text values
+echo $textValues['heading_title'];
+echo $textValues['text_image'];
+echo $textValues['text_clear'];
 
