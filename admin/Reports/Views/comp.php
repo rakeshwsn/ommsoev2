2bcc6050a5c6e303581cfe2b73a887d0440fa2fd
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:59 +0530

    improve

--- admin/Reports/Views/comp.php
+++ admin/Reports/Views/comp.php
@@ -1,29 +1,28 @@
-
 <table class="table table-bordered table-striped table-vcenter js-dataTable-full">
     <thead>
-    <tr>
-        <th>District</th>
-        <th>Block</th>
-        <th>Year</th>
-        <?php if($periodic): ?>
-            <th>Period</th>
-        <?php else: ?>
-            <th>Month</th>
-        <?php endif; ?>
-        <th>Fund Agency</th>
-    </tr>
+        <tr>
+            <th>District</th>
+            <th>Block</th>
+            <th>Year</th>
+            <?php if($periodic): ?>
+                <th>Period</th>
+            <?php else: ?>
+                <th>Month</th>
+            <?php endif; ?>
+            <th>Fund Agency</th>
+        </tr>
     </thead>
     <tbody>
-    <tr>
-        <td><?=$district?></td>
-        <td><?=$block?></td>
-        <td><?=$fin_year?></td>
-        <?php if($periodic): ?>
-            <td><?=$period?></td>
-        <?php else: ?>
-            <td><?=$month_name?></td>
-        <?php endif; ?>
-        <td><?=$fund_agency?></td>
-    </tr>
+        <tr>
+            <td><?= htmlspecialchars($district) ?></td>
+            <td><?= htmlspecialchars($block) ?></td>
+            <td><?= htmlspecialchars($fin_year) ?></td>
+            <?php if($periodic): ?>
+                <td><?= htmlspecialchars($period) ?></td>
+            <?php else: ?>
+                <td><?= htmlspecialchars($month_name) ?></td>
+            <?php endif; ?>
+            <td><?= htmlspecialchars($fund_agency) ?></td>
+        </tr>
     </tbody>
 </table>
