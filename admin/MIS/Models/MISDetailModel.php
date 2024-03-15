d9f3e8ec36b01e75fa65e02275b5bf575a3429cb
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:55 +0530

    improve

--- admin/MIS/Models/MISDetailModel.php
+++ admin/MIS/Models/MISDetailModel.php
@@ -1,24 +1,23 @@
-<?php 
+<?php
+
 namespace Admin\MIS\Models;
+
 use CodeIgniter\Model;
 
 class MISDetailModel extends Model
 {
     protected $table = 'mis_submission_details';
-    protected $primaryKey           = 'id';
-    protected $useAutoIncrement     = true;
-    protected $insertID             = 0;
-    protected $returnType           = 'object';
-    protected $useSoftDeletes        = true;
-    protected $protectFields        = false;
-//	protected $allowedFields        = [];
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID = 0;
+    protected $returnType = 'object';
+    protected $useSoftDeletes = true;
+    protected $protectFields = false;
+    protected $allowedFields = ['column1', 'column2', 'column3']; // add allowed fields here
 
-    // Dates
-    protected $useTimestamps        = true;
-    protected $dateFormat           = 'datetime';
-    protected $createdField         = '';
-    protected $updatedField         = '';
-    protected $deletedField         = 'deleted_at';
-
-
+    protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
 }
