9cd8f8b12dfe45ab0621f39123f9fad3407d8b66
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:53 +0530

    improve

--- admin/MIS/Models/MISFileModel.php
+++ admin/MIS/Models/MISFileModel.php
@@ -1,24 +1,26 @@
-<?php 
+<?php
+
 namespace Admin\MIS\Models;
+
 use CodeIgniter\Model;
 
 class MISFileModel extends Model
 {
     protected $table = 'mis_submission_files';
-    protected $primaryKey           = 'id';
-    protected $useAutoIncrement     = true;
-    protected $insertID             = 0;
-    protected $returnType           = 'object';
-    protected $useSoftDeletes        = true;
-    protected $protectFields        = false;
-//	protected $allowedFields        = [];
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID = 0;
+    protected $returnType = 'object';
+    protected $useSoftDeletes = true;
+    protected $protectFields = false;
+    protected $allowedFields = ['column1', 'column2', 'column3']; // add allowed columns here
 
     // Dates
-    protected $useTimestamps        = true;
-    protected $dateFormat           = 'datetime';
-    protected $createdField         = 'created_at';
-    protected $updatedField         = 'updated_at';
-    protected $deletedField         = 'deleted_at';
+    protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
 
-
+    // Add any custom methods or functionality here
 }
