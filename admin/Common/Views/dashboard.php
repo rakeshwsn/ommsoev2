d5c0a5d35f7cdc7f1c70f01f6d4dd0384136d8f1
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:54 +0530

    improve

--- admin/Common/Views/dashboard.php
+++ admin/Common/Views/dashboard.php
@@ -1,15 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Highcharts Example</title>
+    <?php js_start(); ?>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/highcharts-more.js"></script>
+    <?php js_end(); ?>
+</head>
+<body>
+    <div id="container" class="content"></div>
 
-<div class="content">
-
-</div>
-
-<?php js_start(); ?>
-<script src="https://code.highcharts.com/highcharts.js"></script>
-<script src="https://code.highcharts.com/highcharts-more.js"></script>
-<script type="text/javascript">
-
-</script>
-<?php js_end(); ?>
-
-
-                
+    <?php js_start(); ?>
+    <script type="text/javascript">
+        // Add your Highcharts configuration here
+        Highcharts.chart('container', {
+            title: {
+                text: 'My Chart'
+            },
+            series: [{
+                data: [1, 2, 3, 4, 5]
+            }]
+        });
+    </script>
+    <?php js_end(); ?>
+</body>
+</html>
