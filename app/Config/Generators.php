ccb9647c3728cb81658226fa9d675676c87fddd9
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:15 +0530

    improve

--- app/Config/Generators.php
+++ app/Config/Generators.php
@@ -6,7 +6,21 @@ use CodeIgniter\Config\BaseConfig;
 
 class Generators extends BaseConfig
 {
-	/**
-	 * --------------------------------------------------------------------------
-	 * Generator Command View Mappings
-	 * --------------------------------
+    /**
+     * --------------------------------------------------------------------------
+     * Generator Command View Mappings
+     * ---------------------------------
+     *
+     * Map the generator commands to their corresponding view files.
+     *
+     * @var array
+     */
+    public array $views = [
+        'scaffolding' => 'Generators\scaffolding_view',
+        'controller'  => 'Generators\controller_view',
+        'model'       => 'Generators\model_view',
+        'migration'   => 'Generators\migration_view',
+        'seed'        => 'Generators\seed_view',
+        'entity'      => 'Generators\entity_view',
+    ];
+}
