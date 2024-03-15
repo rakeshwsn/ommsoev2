ab8e82ada1f846f694276edd54b095b9eb07f91d
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:50 +0530

    improve

--- admin/Common/Controllers/Header.php
+++ admin/Common/Controllers/Header.php
@@ -1,43 +1,41 @@
 <?php
+
 namespace Admin\Common\Controllers;
+
 use App\Controllers\AdminController;
-use Admin\Common\Controllers\Leftbar;
 
 class Header extends AdminController
 {
-	public function __construct()
+    /**
+     * @var \Config\Settings
+     */
+    protected $settings;
+
+    /**
+     * Header constructor.
+     */
+    public function __construct()
     {
-		$this->settings = new \Config\Settings();
-	}
-	public function index()
-	{
-		$data=array();
-		
-		$data['site_name'] = $this->settings->config_site_title;
-		if (is_file(DIR_UPLOAD . $this->settings->config_site_logo)) {
-			$data['logo'] = base_url('storage/uploads') . '/' . $this->settings->config_site_logo;
-		} else {
-			$data['logo'] = '';
-		}
-		$data['logout'] = admin_url('logout');
-		if($this->session->get('temp_user')){
-		    $data['relogin']=true;
-        }else {
-            $data['relogin']=false;
+        $this->settings = config('Settings');
+    }
+
+    /**
+     * @return string
+     */
+    public function index(): string
+    {
+        $data = [
+            'site_name' => $this->settings->config_site_title,
+            'logo' => is_file(DIR_UPLOAD . $this->settings->config_site_logo) ? BASE_URL . 'storage/uploads/' . $this->settings->config_site_logo : '',
+            'logout' => admin_url('logout'),
+            'relogin' => session('temp_user') !== null,
+            'name' => user()->getFirstName(),
+        ];
+
+        if (user()->isLogged()) {
+            $data['menu'] = menu('admin', ['menu_class' => 'nav-main']);
         }
-		$data['name']=$this->user->getFirstName();
-		if($this->user->isLogged()){
-			//$leftbar = new Leftbar(); // Create an instance
-			//$data['menu']=$leftbar->index();
-            $menu = new \App\Libraries\Menu(); // Create an instance
-            $data['menu']=$menu->nav_menu([
-                'theme_location'=>'admin',
-                'menu_class'     => 'nav-main'
-            ]);
-		}
 
-
-		return view('Admin\Common\Views\header',$data);
-		
-	}
+        return view('Admin\Common\Views\header', $data);
+    }
 }
