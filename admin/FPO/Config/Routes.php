b8565f79d983352a6051a859012c86b374e24ce9
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:57 +0530

    improve

--- admin/FPO/Config/Routes.php
+++ admin/FPO/Config/Routes.php
@@ -1,17 +1,21 @@
 <?php
+
 namespace Admin\Proceeding\Config;
-if(!isset($routes))
-{
-    $routes = \Config\Services::routes(true);
+
+use Config\Services;
+
+if (!isset($routes)) {
+    $routes = Services::routes(true);
 }
 
-$routes->group(env('app.adminRoute'), ['namespace' => 'Admin','filter' => 'login'], function($routes)
-{
-    $routes->add('fpo','FPO\Controllers\FPO::index');
-    $routes->add('fpo/add','FPO\Controllers\FPO::add');
-    $routes->add('fpo/edit/(:num)','FPO\Controllers\FPO::edit/$1');
-    $routes->add('fpo/details/(:num)','FPO\Controllers\FPO::details/$1');
-    $routes->add('fpo/gedit/(:num)','FPO\Controllers\FPO::gedit/$1');
-    $routes->post('fpo/upload','FPO\Controllers\FPO::upload');
-
+$routes->group(env('app.adminRoute'), [
+    'namespace' => 'Admin',
+    'filter' => 'login'
+], function ($routes) {
+    $routes->add('fpo', 'FPO\Controllers\FPO::index');
+    $routes->add('fpo/create', 'FPO\Controllers\FPO::add');
+    $routes->add('fpo/edit/(:num)', 'FPO\Controllers\FPO::edit/$1');
+    $routes->add('fpo/view/(:num)', 'FPO\Controllers\FPO::details/$1');
+    $routes->add('fpo/geview/(:num)', 'FPO\Controllers\FPO::gedit/$1');
+    $routes->post('fpo/upload', 'FPO\Controllers\FPO::upload');
 });
