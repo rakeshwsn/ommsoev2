2538fcb4d6bc771bfc91ebdcb46c4c97e1bc5d31
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:07 +0530

    improve

--- app/Config/Settings.php
+++ app/Config/Settings.php
@@ -1,35 +1,56 @@
 <?php
+
 namespace Config;
 
 use CodeIgniter\Config\BaseConfig;
+use CodeIgniter\Database\Exception;
+use CodeIgniter\Database\Query;
 
 class Settings extends BaseConfig
 {
-
+    /** @var string */
     const TABLE = 'config';
+
+    /** @var \CodeIgniter\Database\ConnectionInterface */
     private $db;
 
-    function __construct()
+    /**
+     * Settings constructor.
+     */
+    public function __construct()
     {
         $this->db = \Config\Database::connect();
-        $this->get_all();
-
+        try {
+            $this->get_all();
+        } catch (Exception $e) {
+            // Handle the exception or log it
+            // for better error handling and debugging
+            exit('Error fetching settings: ' . $e->getMessage());
+        }
     }
 
-
-    function get_all()
+    /**
+     * @return array
+     */
+    public function get_all(): array
     {
         $builder = $this->db->table(self::TABLE);
-        $Settings = $builder->get();
+        $query = $builder->get();
 
-        foreach ($Settings->getResult() as $Setting) {
-            if (!$Setting->serialized) {
-                $this->{$Setting->key} = $Setting->value;//set value
+        $settings = [];
+
+        foreach ($query->getResult() as $setting) {
+            $key = $setting->key;
+            $value = $setting->value;
+            $serialized = $setting->serialized;
+
+            if (!$serialized) {
+                $settings[$key] = $value;
             } else {
-                $this->{$Setting->key} = json_decode($Setting->value, true);
+                $settings[$key] = json_decode($value, true) ?? [];
             }
-
         }
 
+        return $settings;
     }
 }
