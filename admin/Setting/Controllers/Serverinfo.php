0b90311479ee54fb66f5df254181936c5166038b
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:02 +0530

    improve

--- admin/Setting/Controllers/Serverinfo.php
+++ admin/Setting/Controllers/Serverinfo.php
@@ -1,53 +1,68 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-class Serverinfo extends Admin_Controller {
-	private $error = array();
-	
-	function __construct(){
-      parent::__construct();
-	}
-	public function index(){
-		// Init
-      $data = array();
-		$this->lang->load('serverinfo');
-		$this->template->set_meta_title($this->lang->line('heading_title'));
-		$this->template->add_stylesheet('modules/setting/assets/css/serverinfo.css');
-		
-		$data['breadcrumbs'] = array();
-		$data['breadcrumbs'][] = array(
-			'text' => $this->lang->line('text_settings'),
-			'href' => base_url('setting')
-		);
-		$data['breadcrumbs'][] = array(
-			'text' => $this->lang->line('heading_title'),
-			'href' => base_url('setting/serverinfo')
-		);
-		
-		$data['heading_title'] 	= $this->lang->line('heading_title');
-		ob_start();
-      phpinfo();
-      $pinfo = ob_get_contents();
-      ob_end_clean(); 
-      $data['phpinfo'] = preg_replace( '%^.*<body>(.*)</body>.*$%ms','$1',$pinfo);
+<?php
 
-      $this->template->view('serverinfo', $data);
-	}
-	
-		
-	public function server_info(){
-		$data = array();
-        $data['breadcrumb'] = set_crumbs(array('setting' => 'Settings',current_url() => 'Server Info'));
-        $this->template->add_stylesheet('/admin/modules/setting/assets/css/server_info.css');
+namespace App\Controllers\Admin;
 
+use App\Controllers\BaseController;
+
+class Serverinfo extends BaseController
+{
+    private $error = [];
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+        // Load the language file
+        $this->lang->load('serverinfo');
+
+        // Set the meta title and add the CSS file
+        $this->template->set_meta_title($this->lang->line('heading_title'));
+        $this->template->add_stylesheet('modules/setting/assets/css/serverinfo.css');
+
+        // Set the breadcrumbs
+        $data['breadcrumbs'] = [];
+        $data['breadcrumbs'][] = [
+            'text' => $this->lang->line('text_settings'),
+            'href' => base_url('setting')
+        ];
+        $data['breadcrumbs'][] = [
+            'text' => $this->lang->line('heading_title'),
+            'href' => base_url('setting/serverinfo')
+        ];
+
+        // Set the page heading
+        $data['heading_title'] = $this->lang->line('heading_title');
+
+        // Get the phpinfo() output
+        $data['phpinfo'] = $this->getPhpInfo();
+
+        // Render the view
+        if ($this->template->view('serverinfo', $data)) {
+            return true;
+        }
+
+        // Display an error message if the view failed to render
+        show_error('Failed to render the server info view.');
+    }
+
+    private function getPhpInfo(): string
+    {
         ob_start();
         phpinfo();
         $pinfo = ob_get_contents();
         ob_end_clean();
-         
-        $data['pinfo'] = preg_replace( '%^.*<body>(.*)</body>.*$%ms','$1',$pinfo);
 
-        $this->template->view('server_info', $data);
-	}
-	
-}
-/* End of file hmvc.php */
-/* Location: ./application/widgets/hmvc/controllers/hmvc.php */
+        // Improved regular expression to extract the body of the phpinfo() output
+        return preg_replace('/^.*<body([^>]*)>(.*)<\/body>.*$/s', '$2', $pinfo);
+    }
+
+    public function server_info()
+    {
+        // Load the language file
+        $this->lang->load('serverinfo');
+
+        // Set the breadcrumb
+        $data['
