2cd40cf620e7741d8b8d2a03a4ab8006c35ee52b
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:01 +0530

    improve

--- admin/Common/Views/abstract_table.php
+++ admin/Common/Views/abstract_table.php
@@ -12,14 +12,16 @@
     </thead>
     <tbody>
         <?php
-        if (is_array($rows) && count($rows) > 0) {
+        if (!empty($rows) && is_array($rows) && count($rows) > 0) {
             foreach ($rows as $row) {
                 // Validate and sanitize input
-                $sl = (isset($row['sl']) && is_numeric($row['sl'])) ? $row['sl'] : '';
-                $agency = (isset($row['agency']) && !empty($row['agency'])) ? $row['agency'] : '';
-                $ob_in_lakh = (isset($row['ob_in_lakh']) && is_numeric($row['ob_in_lakh'])) ? $row['ob_in_lakh'] : '';
-                $fr_in_lakh = (isset($row['fr_in_lakh']) && is_numeric($row['fr_in_lakh'])) ? $row['fr_in_lakh'] : '';
-                $ex_in_lakh = (isset($row['ex_in_lakh']) && is_numeric($row['ex_in_lakh'])) ? $row['ex_in_lakh'] : '';
-                $cb_in_lakh = (isset($row['cb_in_lakh']) && is_numeric($row['cb_in_lakh'])) ? $row['cb_in_lakh'] : '';
-                $percentage = (isset($row['percentage']) && is_numeric($row['percentage'])) ? $row['percentage'] : 0;
-                $bg_color = (isset($row['bg_color']) && !empty($row['bg_color']) && preg_match('/^#([A-Fa-f
+                $sl = filter_var($row['sl'], FILTER_VALIDATE_INT) ? $row['sl'] : '';
+                $agency = filter_var($row['agency'], FILTER_SANITIZE_STRING) ?: '';
+                $ob_in_lakh = filter_var($row['ob_in_lakh'], FILTER_VALIDATE_INT) ? $row['ob_in_lakh'] : '';
+                $fr_in_lakh = filter_var($row['fr_in_lakh'], FILTER_VALIDATE_INT) ? $row['fr_in_lakh'] : '';
+                $ex_in_lakh = filter_var($row['ex_in_lakh'], FILTER_VALIDATE_INT) ? $row['ex_in_lakh'] : '';
+                $cb_in_lakh = filter_var($row['cb_in_lakh'], FILTER_VALIDATE_INT) ? $row['cb_in_lakh'] : '';
+                $percentage = filter_var($row['percentage'], FILTER_VALIDATE_INT) ?: 0;
+
+                // Check if bg_color is a valid hex color
+                $bg_color = isset($row['bg_color']) && !empty($row['bg_color']) && preg_match('/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/', $row['bg_color']) ? $row
