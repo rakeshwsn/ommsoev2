0ad98afcf40b7c49797094704787700cbfccf02f
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:39 +0530

    improve

--- front/Common/Views/head.php
+++ front/Common/Views/head.php
@@ -3,6 +3,9 @@ $base_url = base_url();
 $theme_url = theme_url();
 $template = service('template');
 $user = service('user');
+
+$base_url_constant = constant('BASE_URL');
+$theme_url_constant = constant('THEME_URL');
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,40 +15,40 @@ $user = service('user');
 	<?php echo $template->metadata() ?>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!--===============================================================================================-->
-	<link rel="icon" type="image/png" href="<?php echo $theme_url('assets/images/icons/favicon.png'); ?>"/>
+	<link rel="icon" type="image/png" href="<?php echo e($theme_url('assets/images/icons/favicon.png')); ?>"/>
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/vendor/bootstrap/css/bootstrap.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/vendor/bootstrap/css/bootstrap.min.css')); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css')); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/fonts/linearicons-v1.0.0/icon-font.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/fonts/linearicons-v1.0.0/icon-font.min.css')); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/vendor/animate/animate.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/vendor/animate/animate.css')); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/vendor/css-hamburgers/hamburgers.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/vendor/css-hamburgers/hamburgers.min.css')); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/vendor/animsition/css/animsition.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/vendor/animsition/css/animsition.min.css')); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/vendor/select2/select2.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/vendor/select2/select2.min.css')); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/vendor/daterangepicker/daterangepicker.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/vendor/daterangepicker/daterangepicker.css')); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/vendor/slick/slick.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/vendor/slick/slick.css')); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/vendor/lightbox2/css/lightbox.min.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/vendor/lightbox2/css/lightbox.min.css')); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/vendor/perfect-scrollbar/perfect-scrollbar.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/vendor/perfect-scrollbar/perfect-scrollbar.css')); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/vendor/revolution/css/layers.css'); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/vendor/revolution/css/navigation.css'); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/vendor/revolution/css/settings.css'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/vendor/revolution/css/layers.css')); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/vendor/revolution/css/navigation.css')); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/vendor/revolution/css/settings.css')); ?>">
 	<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/css/util.css'); ?>">
-	<link rel="stylesheet" type="text/css" href="<?php echo $theme_url('assets/css/main.css?v=1'); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/css/util.css')); ?>">
+	<link rel="stylesheet" type="text/css" href="<?php echo e($theme_url('assets/css/main.css?v=1')); ?>">
 
-	<script type="text/javascript">
-		var BASE_URL = '<?php echo $base_url; ?>';
-		var THEME_URL = '<?php echo $theme_url; ?>';
+	<script>
+		const BASE_URL = "<?= $base_url_constant ?>";
+		const THEME_URL = "<?= $theme_url_constant ?>";
 	</script>
 
 	<?php echo $template->stylesheets() ?>
@@ -67,4 +70,3 @@ $user = service('user');
 					<a href="<?= $base_url ?>" class="align-self-center">
 					<h2>Special Programme for Promotion of Integrated Farming</h2>
 					<!--<img src="images/sppif-text-logo.png" class="img-fluid" alt="IMG-LOGO">-->
-	
