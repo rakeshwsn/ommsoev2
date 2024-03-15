51832c5ee20ad91c4272dd15e79842fd573fc6c6
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:12 +0530

    improve

--- front/Banner/Models/ProceedingModel.php
+++ front/Banner/Models/ProceedingModel.php
@@ -6,47 +6,48 @@ use CodeIgniter\Model;
 
 class ApiModel extends Model
 {
-	protected $DBGroup              = '';
-	protected $table                = 'apis';
-	protected $primaryKey           = 'id';
-	protected $useAutoIncrement     = true;
-	protected $insertID             = 0;
-	protected $returnType           = 'array';
-	protected $useSoftDeletes       = false;
-	protected $protectFields        = true;
-	protected $allowedFields        = [];
+    protected $DBGroup              = 'default'; // Specify the database group
+    protected $table                = 'apis';
+    protected $primaryKey           = 'id';
+    protected $useAutoIncrement     = true;
+    protected $returnType           = 'array';
+    protected $useSoftDeletes       = false;
+    protected $protectFields        = true;
+    protected $allowedFields        = [];
 
-	// Dates
-	protected $useTimestamps        = false;
-	protected $dateFormat           = 'datetime';
-	protected $createdField         = 'created_at';
-	protected $updatedField         = 'updated_at';
-	protected $deletedField         = 'deleted_at';
+    // Dates
+    protected $useTimestamps        = false;
+    protected $dateFormat           = 'datetime';
+    protected $createdField         = 'created_at';
+    protected $updatedField         = 'updated_at';
+    protected $deletedField         = 'deleted_at';
 
-	// Validation
-	protected $validationRules      = [];
-	protected $validationMessages   = [];
-	protected $skipValidation       = false;
-	protected $cleanValidationRules = true;
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
 
-	// Callbacks
-	protected $allowCallbacks       = true;
-	protected $beforeInsert         = [];
-	protected $afterInsert          = [];
-	protected $beforeUpdate         = [];
-	protected $afterUpdate          = [];
-	protected $beforeFind           = [];
-	protected $afterFind            = [];
-	protected $beforeDelete         = [];
-	protected $afterDelete          = [];
-	
-	public function getXform($form_id) {
-        $sql = "SELECT *
-FROM _form_info fi
-  LEFT JOIN _form_info_xform_bin fixb
-    ON fi._URI = fixb._TOP_LEVEL_AURI
-	WHERE fi.FORM_ID = '".$form_id."'";
+    // Callbacks
+    protected $allowCallbacks       = true;
+    protected $beforeInsert         = [];
+    protected $afterInsert          = [];
+    protected $beforeUpdate         = [];
+    protected $afterUpdate          = [];
+    protected $beforeFind           = [];
+    protected $afterFind            = [];
+    protected $beforeDelete         = [];
+    protected $afterDelete          = [];
 
-        return $this->db->query($sql)->getRowArray();
-	}
+    public function getXform($form_id)
+    {
+        // Use the query builder to prevent SQL injection attacks
+        return $this->builder()
+            ->select('*')
+            ->from('_form_info fi')
+            ->join('_form_info_xform_bin fixb', 'fi._URI = fixb._TOP_LEVEL_AURI', 'left')
+            ->where('fi.FORM_ID', $form_id)
+            ->get()
+            ->getRowArray();
+    }
 }
