1b9a14cfcb96082589351b27df39fa02771e63d1
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:52 +0530

    improve

--- admin/Common/Views/ps_dashboard.php
+++ admin/Common/Views/ps_dashboard.php
@@ -1,20 +1,2 @@
 <div class="content">
-    <div class="row invisible" data-toggle="appear">
-        <!-- Row #1 -->
-        <div class="col-6 col-xl-3">
-            <a class="block block-rounded block-bordered block-link-shadow" href="javascript:void(0)">
-                <div class="block-content block-content-full clearfix">
-                    <div class="float-right mt-15 d-none d-sm-block">
-                        <i class="si si-login fa-2x text-earth-light" alt="Fund Receipt"></i>
-                    </div>
-                    <div class="font-size-h3 font-w600 text-earth"><i class="fa fa-rupee"></i> <span data-toggle="countTo" data-speed="1000" data-to="<?= $fr ?>">0</span></div>
-                    <div class="font-size-sm font-w600 text-uppercase text-muted">Fund Receipt</div>
-                </div>
-            </a>
-        </div>
-        <div class="col-6 col-xl-3">
-            <a class="block block-rounded block-bordered block-link-shadow" href="javascript:void(0)">
-                <div class="block-content block-content-full clearfix">
-                    <div class="float-right mt-15 d-none d-sm-block">
-                        <i class="si si-logout fa-2x text-elegance-light" alt="Expense"></i>
-                
+    <div class
