764050539878b44c63f6e36eee8b5e278c4bedea
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:12 +0530

    improve

--- admin/Budgets/Views/budgetPlanForm.php
+++ admin/Budgets/Views/budgetPlanForm.php
@@ -7,36 +7,37 @@ $validation = \Config\Services::validation();
             <div class="block-header block-header-default">
                 <h3 class="block-title"><?php echo $text_form; ?></h3>
                 <div class="block-options">
-                    <button type="submit" data-toggle="tooltip" title="" class="btn btn-danger" form="form-component"><i class="fa fa-save"></i></button>
-                    <a href="<?php echo $cancel; ?>" data-toggle="tooltip" title="" class="btn btn-primary"><i class="fa fa-reply"></i></a>
+                    <button type="submit" name="submit" data-toggle="tooltip" title="Save" class="btn btn-danger" form="form-component"><i class="fa fa-save"></i></button>
+                    <label class="btn btn-primary" for="cancel-button">Cancel</label>
+                    <a id="cancel-button" href="<?php echo $cancel; ?>" data-toggle="tooltip" title="Cancel" class="d-none"></a>
                 </div>
             </div>
             <div class="block-content">
-                <?php echo form_open_multipart('',array('class' => 'form-horizontal', 'id' => 'form-component','role'=>'form')); ?>
+                <?php echo form_open_multipart('',array('class' => 'form-horizontal', 'id' => 'form-component', 'name' => 'form-component', 'autocomplete' => 'off', 'novalidate')) ?>
                 <div class="form-group row">
                     <label class="col-sm-2 control-label" for="input-status">Fund Agency</label>
                     <div class="col-sm-10">
-                        <?php echo form_dropdown('fund_agency_id', option_array_values($fund_agencies, 'fund_agency_id', 'fund_agency'), set_value('fund_agency_id', $fund_agency_id), "id='fund_agency_id' class='form-control'")?>
+                        <?php echo form_dropdown('fund_agency_id', option_array_values($fund_agencies, 'fund_agency_id', 'fund_agency'), set_value('fund_agency_id', $fund_agency_id), array('id' => 'fund_agency_id', 'class' => 'form-control', 'required' => 'required', 'autofocus' => 'autofocus', 'aria-required' => 'true'))?>
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-2 control-label" for="input-category">Year</label>
                     <div class="col-sm-10">
-                        <?php echo form_dropdown('year', option_array_value($years, 'id', 'name'), set_value('year', $year),"id='year' class='form-control'"); ?>
+                        <?php echo form_dropdown('year', option_array_value($years, 'id', 'name'), set_value('year', $year), array('id' => 'year', 'class' => 'form-control', 'required' => 'required', 'aria-required' => 'true')) ?>
                     </div>
                 </div>
                 <div class="form-group row">
                     <label class="col-sm-2 control-label" for="input-status">District</label>
                     <div class="col-sm-10">
-                        <?php echo form_dropdown('district_id', option_array_value($districts, 'id', 'name',['0'=>'Select District']), set_value('district_id', $district_id), "id='district_id' class='form-control'")?>
+                        <?php echo form_dropdown('district_id', option_array_value($districts, 'id', 'name', ['0' => 'Select District']), set_value('district_id', $district_id), array('id' => 'district_id', 'class' => 'form-control', 'required' => 'required', 'aria-required' => 'true')) ?>
                     </div>
                 </div>
 
                 <div class="form-group row">
                     <label class="col-sm-2 control-label" for="input-status">Block</label>
                     <div class="col-sm-10">
-                        <?php echo form_dropdown('block_id', array(), set_value('block_id', $block_id), "id='block_id' class='form-control'")?>
+                        <?php echo form_dropdown('block_id', array(), set_value('block_id', $block_id), array('id' => 'block_id', 'class' => 'form-control', 'aria-required' => 'true')) ?>
                     </div>
                 </div>
 
@@ -77,17 +78,4 @@ $validation = \Config\Services::validation();
                         html += '<option value="0" selected="selected">Select Block</option>';
                     }
 
-                    $('select[name=\'block_id\']').html(html);
-                    $('select[name=\'block_id\']').select2();
-                },
-                error: function(xhr, ajaxOptions, thrownError) {
-                    alert(thrownError + "\r\n" + xhr.statusText + "\r\n" + xhr.responseText);
-                }
-            });
-        });
-        $('select[name=\'district_id\']').trigger('change');
-    });
-    
-</script>
-<?php js_end(); ?>
-
+                    $('
