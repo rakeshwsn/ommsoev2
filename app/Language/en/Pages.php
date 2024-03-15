457cc549984eb2f4bd88143264fb28aa9f1e103b
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:21 +0530

    improve

--- app/Language/en/Pages.php
+++ app/Language/en/Pages.php
@@ -1,10 +1,19 @@
 <?php
-// Text
-return [
-	'heading_title'                	=> 'Pages',
-	'text_image'           => 'Select Image',
-	'text_clear'            => 'Clear'
+
+// Define the array of text values
+$text = [
+	'heading_title'                 => 'Pages',
+	'text_image'                    => 'Select Image',
+	'text_clear'                     => 'Clear'
 ];
 
+// Return the array of text values
+function getTextValues(): array
+{
+	global $text;
+	return $text;
+}
 
+// Call the function to get the text values
+getTextValues();
 
