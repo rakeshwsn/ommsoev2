40a7ecf8da9e75e4dc60cb3cc29d53ddc4684377
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:06 +0530

    improve

--- app/Language/en/Village.php
+++ app/Language/en/Village.php
@@ -1,12 +1,15 @@
 <?php
-// Text
-return [
-	'heading_title'        	=> 'Village',
-	'text_list'           	=> 'Village List',
-	'text_edit'            	=> 'Edit Village',
-	'text_add'            	=> 'Add Village'
+// An array to store text strings for the Village module
+$text = [
+    'heading_title'        	=> 'Village', // The title of the page
+    'text_list'           	=> 'Village List', // The title of the list page
+    'text_edit'            	=> 'Edit Village', // The title of the edit page
+    'text_add'            	=> 'Add Village' // The title of the add page
 ];
 
-
-
-
+// A function to return the text array
+function getText() {
+    global $text;
+    return $text;
+}
+?>
