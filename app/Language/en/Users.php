bde23b830898f448f26adeafbbbda596192c464e
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:17 +0530

    improve

--- app/Language/en/Users.php
+++ app/Language/en/Users.php
@@ -1,10 +1,17 @@
 <?php
-// Text
-return [
-	'heading_title'        => 'Users',
-	'text_image'           => 'Select Image',
-	'text_clear'            => 'Clear'
+
+// Define the array of text values
+$text = [
+    'heading_title'        => 'Users',
+    'text_image'           => 'Select Image',
+    'text_clear'            => 'Clear'
 ];
 
+// Return the array of text values
+function getTextValues() {
+    global $text;
+    return $text;
+}
 
-
+// Call the function to get the text values
+getTextValues();
