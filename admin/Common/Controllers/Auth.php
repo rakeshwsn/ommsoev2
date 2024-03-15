a6b00855b426ba21306a2bce30c558fa26516c99
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:10 +0530

    improve

--- admin/Common/Controllers/Auth.php
+++ admin/Common/Controllers/Auth.php
@@ -1,64 +1,102 @@
 <?php
+
 namespace Admin\Common\Controllers;
+
 use App\Controllers\AdminController;
 use App\Libraries\User;
 use CodeIgniter\HTTP\CURLRequest;
+use CodeIgniter\HTTP\RedirectResponse;
+use CodeIgniter\Session\Session;
 
 class Auth extends AdminController
 {
-	public function __construct()
+    /**
+     * Auth constructor.
+     */
+    public function __construct()
     {
-		$this->uri = service('uri');
-	}
-	public function login() {
-        $data['login_error']='';
+        $this->session = service('session');
+        $this->user = new User();
+    }
 
-        if($this->user->isLogged()){
+    /**
+     * Login method for user authentication.
+     *
+     * @return RedirectResponse|void
+     */
+    public function login(): void
+    {
+        $data['login_error'] = '';
+
+        if ($this->user->isLogged()) {
             return redirect()->to('/admin');
         }
-        if($this->request->getMethod(1)=='POST'){
+
+        if ($this->request->getMethod('post')) {
             $logged_in = $this->user->login(
                 $this->request->getPost('username'),
                 $this->request->getPost('password')
             );
-            if($logged_in){
-				if ($this->request->getPost('redirect') && (strpos($this->request->getPost('redirect'), admin_url()) == 0 )) {
-					return redirect()->to($this->request->getPost('redirect'));
-				} else{
-					return redirect()->to(base_url('admin'));
-				}
+
+            if ($logged_in) {
+                $redirect = $this->request->getPost('redirect') ?? '';
+
+                if ($redirect && strpos(uri_string(), admin_url()) === 0) {
+                    return redirect()->to($redirect);
+                } else {
+                    return redirect()->to(site_url('admin'));
+                }
             } else {
-                $data['login_error']=$this->session->getFlashdata('error');
+                $data['login_error'] = $this->session->getFlashdata('error') ?? '';
             }
         }
 
-		if($this->uri->getTotalSegments() > 0){
-			$route=uri_string();
-			$data['redirect'] = $route;
-		} else {
-			$data['redirect'] = '';
-		}
+        $data['redirect'] = uri_string();
 
-		return $this->template->view('Admin\Common\Views\login',$data);
-        //return view('common/login',$data);
-	}
-
-	public function logout() {
-        $this->user->logout();
-        $this->session->remove('redirect');
-        return redirect()->to('/admin');
-	}
-
-	public function reLogin(){
-        $user = $this->session->get('temp_user');
-        $this->session->set('user',$user);
-        $this->user->assignUserAttr($user);
-        $this->session->remove('temp_user');
-        return redirect()->to(base_url('admin'));
+        echo $this->template->view('Admin\Common\Views\login', $data);
     }
 
-    public function oldPortalLogin() {
-        $client = \Config\Services::curlrequest();
+    /**
+     * Logout method for user logout.
+     *
+     * @return RedirectResponse
+     */
+    public function logout(): RedirectResponse
+    {
+        $this->user->logout();
+        $this->session->remove('redirect');
+
+        return redirect()->to('/admin');
+    }
+
+    /**
+     * ReLogin method for re-login functionality.
+     *
+     * @return RedirectResponse
+     */
+    public function reLogin(): RedirectResponse
+    {
+        $user = $this->session->get('temp_user');
+
+        if ($user) {
+            $this->user->assignUserAttr($user);
+            $this->session->set('user', $user);
+            $this->session->remove('temp_user');
+
+            return redirect()->to(site_url('admin'));
+        }
+
+        return redirect()->to('/admin');
+    }
+
+    /**
+     * OldPortalLogin method for old portal login.
+     *
+     * @return RedirectResponse
+     */
+    public function oldPortalLogin(): RedirectResponse
+    {
+        $client = service('curlrequest');
 
         $response = $client->request('POST', 'https://soe1.milletsodisha.com/api/user/username', [
             'headers' => [
@@ -66,24 +104,15 @@ class Auth extends AdminController
             ],
             'form_params' => [
                 'username' => $this->user->getUserName(),
-//                'username' => 'fa_bijepur',
             ]
         ]);
 
         $body = $response->getBody();
-        if (strpos($response->getHeader('content-type'), 'application/json') !== false) {
+
+        if (strpos($response->getHeaderLine('content-type'), 'application/json') !== false) {
             $body = json_decode($body);
         }
 
-        if(isset($body->status) && $body->status){
+        if (isset($body->status) && $body->status) {
             $id = $body->user->id;
-            $password = $body->user->password;
-            return redirect()->to('https://soe1.milletsodisha.com/api/user/login/'.$id.'/'.$password.'?X-API-KEY=4o8c0ow0wooss4kswgwwcs4444swk0oc44gwc8gs');
-        } else {
-            return redirect()->with('message','Cannot login to old portal. Contact Admin')->to('/admin');
-        }
-
-
-
-    }
-}
+            $password
