393c0f0a90be3ca91c1f2196d46427d867e5913c
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:57 +0530

    improve

--- admin/Common/Views/leftbar.php
+++ admin/Common/Views/leftbar.php
@@ -1,44 +1,42 @@
 <ul class="nav-main ss">
-	<?php foreach ($menus as $menu) { ?>
-	<li id="<?php echo $menu['id']; ?>">
-		<?php if ($menu['href']) { ?>
-		<a href="<?php echo $menu['href']; ?>"><i class="<?php echo $menu['icon']; ?> "></i> <span class="sidebar-mini-hide"><?php echo $menu['name']; ?></span></a>
-		<?php } else { ?>
-		<a class="<?php echo $menu['children']?'nav-submenu':'';?>" data-toggle="<?php echo $menu['children']?'nav-submenu':'';?>" href="#"><i class="<?php echo $menu['icon']; ?> "></i> <span class="sidebar-mini-hide"><?php echo $menu['name']; ?></span></a>
-		<?php } ?>
-		<?php if ($menu['children']) { ?>
-		<ul class="list-unstyled">
-			<?php foreach ($menu['children'] as $children_1) { ?>
-			<li>
-				<?php if ($children_1['href']) { ?>
-				<a href="<?php echo $children_1['href']; ?>"><?php echo $children_1['name']; ?></a>
-				<?php } else { ?>
-				<a class="parent waves-effect"><?php echo $children_1['name']; ?></a>
-				<?php } ?>
-				<?php if ($children_1['children']) { ?>
-				<ul>
-					<?php foreach ($children_1['children'] as $children_2) { ?>
-					<li>
-						<?php if ($children_2['href']) { ?>
-						<a href="<?php echo $children_2['href']; ?>"><?php echo $children_2['name']; ?></a>
-						<?php } else { ?>
-						<a class="parent waves-effect"><?php echo $children_2['name']; ?></a>
-						<?php } ?>
-						<?php if ($children_2['children']) { ?>
-						<ul>
-							<?php foreach ($children_2['children'] as $children_3) { ?>
-							<li><a href="<?php echo $children_3['href']; ?>"><?php echo $children_3['name']; ?></a></li>
-							<?php } ?>
-						</ul>
-						<?php } ?>
-					</li>
-					<?php } ?>
-				</ul>
-				<?php } ?>
-			</li>
-			<?php } ?>
-		</ul>
-		<?php } ?>
-	</li>
-	<?php } ?>
-</ul>
+    <?php foreach ($menus as $menu): ?>
+        <?php $submenuClass = $menu['children'] ? 'nav-submenu' : ''; ?>
+        <li id="<?= $menu['id']; ?>">
+            <?php if ($menu['href']): ?>
+                <a href="<?= $menu['href']; ?>" class="<?= $submenuClass; ?>" data-toggle="<?= $submenuClass; ?>">
+                    <i class="<?= $menu['icon']; ?> "></i>
+                    <span class="sidebar-mini-hide"><?= $menu['name']; ?></span>
+                </a>
+            <?php else: ?>
+                <a class="<?= $submenuClass; ?>" data-toggle="<?= $submenuClass; ?>" href="#">
+                    <i class="<?= $menu['icon']; ?> "></i>
+                    <span class="sidebar-mini-hide"><?= $menu['name']; ?></span>
+                </a>
+            <?php endif; ?>
+
+            <?php if ($menu['children']): ?>
+                <ul class="list-unstyled">
+                    <?php foreach ($menu['children'] as $children_1): ?>
+                        <?php $childrenSubmenuClass = $children_1['children'] ? 'parent waves-effect' : ''; ?>
+                        <li>
+                            <?php if ($children_1['href']): ?>
+                                <a href="<?= $children_1['href']; ?>" class="<?= $childrenSubmenuClass; ?>"><?= $children_1['name']; ?></a>
+                            <?php else: ?>
+                                <a class="<?= $childrenSubmenuClass; ?>"><?= $children_1['name']; ?></a>
+                            <?php endif; ?>
+
+                            <?php if ($children_1['children']): ?>
+                                <ul>
+                                    <?php foreach ($children_1['children'] as $children_2): ?>
+                                        <?php $childrenSubmenuClass = $children_2['children'] ? 'parent waves-effect' : ''; ?>
+                                        <li>
+                                            <?php if ($children_2['href']): ?>
+                                                <a href="<?= $children_2['href']; ?>" class="<?= $childrenSubmenuClass; ?>"><?= $children_2['name']; ?></a>
+                                            <?php else: ?>
+                                                <a class="<?= $childrenSubmenuClass; ?>"><?= $children_2['name']; ?></a>
+                                            <?php endif; ?>
+
+                                            <?php if ($children_2['children']): ?>
+                                                <ul>
+                                                    <?php foreach ($children_2['children'] as $children_3): ?>
+                                                        <li
