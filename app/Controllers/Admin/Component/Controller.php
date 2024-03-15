d0637cadb8adefd553e45c21882bf537c317a7fa
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:12 +0530

    improve

--- app/Controllers/Admin/Component/Controller.php
+++ app/Controllers/Admin/Component/Controller.php
@@ -6,34 +6,7 @@ use App\Controllers\BaseController;
 
 class Controller extends BaseController
 {
-    protected $data;
+    protected array $data = [];
 
     public function __construct()
-    {
-        $this->data = [];
-    }
-
-    public function index()
-    {
-        // Set default data
-        $this->data['title'] = 'Dashboard';
-
-        // Load view
-        return view('admin/component/index', $this->data);
-    }
-
-    protected function setData($key, $value)
-    {
-        $this->data[$key] = $value;
-    }
-
-    protected function getData($key = null)
-    {
-        if ($key) {
-            return isset($this->data[$key]) ? $this->data[$key] : null;
-        }
-
-        return $this->data;
-    }
-}
 
