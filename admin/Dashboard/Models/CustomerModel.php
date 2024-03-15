d4d9467d3a9556eba5212f40e6db3dbb6e36fead
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:47 +0530

    improve

--- admin/Dashboard/Models/CustomerModel.php
+++ admin/Dashboard/Models/CustomerModel.php
@@ -1,24 +1,33 @@
-<?php 
+<?php
+
 namespace Admin\Customer\Models;
+
 use CodeIgniter\Model;
 
-class CustomerModel extends Model 
+class CustomerModel extends Model
 {
     protected $table = 'Customer';
-    protected $allowedFields = [];
+    protected $allowedFields = ['column1', 'column2', 'column3']; // add allowed fields
     protected $beforeInsert = ['beforeInsert'];
     protected $beforeUpdate = ['beforeUpdate'];
-    
+
     public function __construct()
     {
         parent::__construct();
     }
-    
-    protected function beforeInsert(array $data) {
+
+    protected function beforeInsert(array $data)
+    {
+        // add any modifications before inserting data here
+        // for example, hashing passwords
+
         return $data;
     }
 
-    protected function beforeUpdate(array $data) {
+    protected function beforeUpdate(array $data)
+    {
+        // add any modifications before updating data here
+
         return $data;
     }
 }
