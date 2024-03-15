d3766eed0c3581eb2b07f13119c9cc2bbb3afef4
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:56 +0530

    improve

--- admin/Budgets/Views/mpr_block.php
+++ admin/Budgets/Views/mpr_block.php
@@ -1,4 +1,18 @@
-    <?php $filter_panel = $this->load->view('filter_panel', array('filter_data' => $filter_data), true); ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Report</title>
+    <?php $this->load->view('header'); ?>
+</head>
+<body>
+
+<section class="content">
+
+    <?php
+    $filter_panel = $this->load->view('filter_panel', array('filter_data' => $filter_data), true);
+    $components = empty($components) ? false : $components;
+    ?>
 
     <?php if ($components): ?>
     <div class="block block-themed">
@@ -7,60 +21,4 @@
             <div class="block-options">
                 <a href="<?= $download_url ?>" class="btn btn-secondary" data-toggle="tooltip" data-original-title="Download">
                     <i class="si si-cloud-download"></i>
-                </a>
-            </div>
-        </div>
-        <div class="block-content block-content-full">
-            <div class="tableFixHead">
-                <table class="table custom-table table-striped table-hover" id="txn-table" data-order='[[ 0, "asc" ]]' data-order-method='original' data-page-length='50'>
-                    <thead>
-                        <tr>
-                            <th rowspan="3">Sl no</th>
-                            <th rowspan="3" data-title="Component">Component</th>
-                            <th rowspan="2" colspan="2">Opening Balance</th>
-                            <th rowspan="2" colspan="2">Target</th>
-                            <th rowspan="1" colspan="6">Allotment received (in lakhs) from DA & FP(O)</th>
-                            <th rowspan="1" colspan="4">Expenditure (in lakhs)</th>
-                            <th rowspan="2" colspan="2">Unspent Balance upto the month (in lakhs)</th>
-                        </tr>
-                        <tr>
-                            <th colspan="2">As per statement upto prev month</th>
-                            <th colspan="2">During the month</th>
-                            <th colspan="2">Upto the month</th>
-                            <th colspan="2">During the month</th>
-                            <th colspan="2">Cumulative Expenditure upto the month</th>
-                        </tr>
-                        <tr>
-                            <th>Phy&nbsp;</th>
-                            <th>Fin&nbsp;</th>
-                            <th>Phy&nbsp;</th>
-                            <th>Fin&nbsp;</th>
-                            <th>Phy&nbsp;</th>
-                            <th>Fin&nbsp;</th>
-                            <th>Phy&nbsp;</th>
-                            <th>Fin&nbsp;</th>
-                            <th>Phy&nbsp;</th>
-                            <th>Fin&nbsp;</th>
-                            <th>Phy&nbsp;</th>
-                            <th>Fin&nbsp;</th>
-                            <th>Phy&nbsp;</th>
-                            <th>Fin&nbsp;</th>
-                            <th>Phy&nbsp;</th>
-                            <th>Fin&nbsp;</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?= $components ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-    <?php endif; ?>
-
-</section>
-
-<script>
-    $(document).ready(function() {
-        $('#txn-table').DataTable({
-            "init
+              
