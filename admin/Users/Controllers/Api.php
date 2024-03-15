e82d8721dc9ea7a03ef5e94c99414a393c87ca3d
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:03 +0530

    improve

--- admin/Users/Controllers/Api.php
+++ admin/Users/Controllers/Api.php
@@ -1,24 +1,31 @@
 <?php
+
 namespace Admin\Users\Controllers;
+
 use Admin\Localisation\Models\BlockModel;
 use App\Controllers\AdminController;
 use Admin\Localisation\Models\DistrictModel;
 use Admin\Users\Models\UserGroupModel;
 use Admin\Users\Models\UserModel;
 
-class Api extends AdminController{
-	private $error = array();
-	private $userModel;
-	
-	public function __construct(){
-		$this->userModel=new UserModel();
-    }
-	
-	public function login(){
-		
+class Api extends AdminController
+{
+    protected $error = [];
+    protected $userModel;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->userModel = new UserModel();
     }
 
+    public function login()
+    {
+        // Add your login logic here
+    }
+
+    // Add other methods as needed
 }
 
-/* End of file hmvc.php */
-/* Location: ./application/widgets/hmvc/controllers/hmvc.php */
+/* End of file Api.php */
+/* Location: ./application/modules/Admin/Users/Controllers/Api.php */
