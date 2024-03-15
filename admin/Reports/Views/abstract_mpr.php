eaa45c92436817054b19fb3195d9db51d35eb4ca
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:59 +0530

    improve

--- admin/Reports/Views/abstract_mpr.php
+++ admin/Reports/Views/abstract_mpr.php
@@ -7,28 +7,10 @@
         <div class="block-header bg-muted">
             <h3 class="block-title">Report</h3>
             <div class="block-options">
-                <a href="<?=$download_url?>" class="btn btn-secondary" data-toggle="tooltip" data-original-title="Download">
+                <a href="<?=$download_url?>" class="btn btn-secondary" data-toggle="tooltip" title="Download" role="button" data-original-title="Download">
                     <i class="si si-cloud-download"></i>
                 </a>
             </div>
         </div>
-        <div class="block-content block-content-full">
-            <div class="tableFixHead">
-                <?=$mpr_table?>
-            </div>
-        </div>
-    </div>
-    <?php endif; ?>
-
-</section>
-<script>
-
-<?php js_start(); ?>
-$(function () {
-    /*var $th = $('.tableFixHead').find('thead th');
-    $('.tableFixHead').on('scroll', function() {
-        $th.css('transform', 'translateY('+ this.scrollTop +'px)');
-    });*/
-});
-<?php js_end(); ?>
-</script>
+        <div class="block-content block-content-full" style="margin-top: 1rem;">
+            <div id="tableFixHead" class="tableFixHead" style="min-width: 100%; box
