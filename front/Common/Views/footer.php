c027bc334a989f7acf3649502f4765e27a09455f
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:25 +0530

    improve

--- front/Common/Views/footer.php
+++ front/Common/Views/footer.php
@@ -1,21 +1,29 @@
-<?php 
-$template=service('template');
-$user=service('user');
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style type="text/css">
+        /* Add your CSS here */
+    </style>
+</head>
+<body>
+
 <!-- FOOTER START -->
-<?php if($header){?>
+<?php if($header):?>
 <footer class="site-footer mt-3">
     <div class="container">
         <div class="row">
             <div class="col-lg-6 footer-col left-col">
-                <img src="images/logos/sppif-header.svg" alt="" class="img-fluid mb-3">
+                <img src="images/logos/sppif-header.svg" alt="SPPIF logo" class="img-fluid mb-3">
                 <p>The SPPIF is for enhancing production, income and household level food security & reducing climate risks will be the focus.</p>
                 <div class="footer-social">
                     <ul>
-                        <li><a href="index.html#"><i class="fab fa-facebook-f"></i></a></li>
-                        <li><a href="index.html#"><i class="fab fa-twitter"></i></a></li>
-                        <li><a href="index.html#"><i class="fab fa-instagram"></i></a></li>
-                        <li><a href="index.html#"><i class="fab fa-youtube"></i></a></li>
+                        <li><a href="https://www.facebook.com/" target="blank" rel="noopener noreferrer"><i class="fab fa-facebook-f" aria-label="Facebook"></i></a></li>
+                        <li><a href="https://www.twitter.com/" target="blank" rel="noopener noreferrer"><i class="fab fa-twitter" aria-label="Twitter"></i></a></li>
+                        <li><a href="https://www.instagram.com/" target="blank" rel="noopener noreferrer"><i class="fab fa-instagram" aria-label="Instagram"></i></a></li>
+                        <li><a href="https://www.youtube.com/" target="blank" rel="noopener noreferrer"><i class="fab fa-youtube" aria-label="YouTube"></i></a></li>
                     </ul>
                 </div>
             </div>
@@ -36,41 +44,11 @@ $user=service('user');
             </div>
             <div class="col-lg-3 footer-col">
                 <h5>NEWSLETTER</h5>
-                <p>Subscribe to our newsletter to receive
-                    the latest news about our services.</p>
+                <p>Subscribe to our newsletter to receive the latest news about our services.</p>
                 <div class="newsletter">
-                    <form action="index.html#" method="post" name="sign-up">
-                        <input type="email" class="input" id="email" name="email" placeholder="Your email address"
-                               required>
-                        <input type="submit" class="button" id="submit" value="SIGN UP">
+                    <form action="/newsletter-signup" method="post" name="sign-up" novalidate>
+                        <label for="newsletter-email">Email address:</label>
+                        <input type="email" class="input" id="newsletter-email" name="email" placeholder="Your email address" required>
+                        <input type="submit" class="button" id="newsletter-submit" value="Sign Up">
                     </form>
-                </div>
-            </div>
-        </div>
-        <hr class="footer">
-        <div class="bottom-footer">
-            <p>© 2021 DAFP, Odisha. All rights reserved.</p>
-        </div>
-    </div>
-</footer>
-<!-- FOOTER END -->
-
-<!--SCROLL TOP START-->
-<a href="index.html#0" class="cd-top">Top</a>
-<!--SCROLL TOP START-->
-<?}?>
-
-<!-- JAVASCRIPTS -->
-<script src="<?php echo theme_url('assets/js/lib/jquery-3.5.1.min.js'); ?>"></script>
-<script src="<?php echo theme_url('assets/js/lib/bootstrap.min.js'); ?>"></script>
-<script src="<?php echo theme_url('assets/js/lib/plugins.js'); ?>"></script>
-<script src="<?php echo theme_url('assets/js/lib/nav.fixed.top.js'); ?>"></script>
-<script src="<?php echo theme_url('assets/js/lib/contact.js'); ?>"></script>
-<script src="<?php echo theme_url('assets/js/slider.js'); ?>"></script>
-<!-- JAVASCRIPTS END -->
-
-<?php echo $template->footer_javascript() ?>
-<script src="<?php echo theme_url('assets/js/main.js'); ?>"></script>
-
-</body>
-</html>
+                </div
