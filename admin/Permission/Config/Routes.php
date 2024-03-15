dace93640972a7d4bcaf6fd537874d7ca47e0f0e
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:54 +0530

    improve

--- admin/Permission/Config/Routes.php
+++ admin/Permission/Config/Routes.php
@@ -1,15 +1,25 @@
 <?php
-if(!isset($routes))
-{ 
+
+if (!isset($routes)) {
     $routes = \Config\Services::routes(true);
 }
-$routes->group('admin', ['namespace' => 'Admin','filter' => 'login'], function($routes)
-{
-    $routes->add('permission', 'Permission\Controllers\Permission::index');
-    $routes->post('permission/search','Permission\Controllers\Permission::search');
-    $routes->match(['get','post'],'permission/add', 'Permission\Controllers\Permission::add');
-    $routes->match(['get','post'],'permission/edit/(:segment)', 'Permission\Controllers\Permission::edit/$1');
-    $routes->get('permission/delete/(:segment)',   'Permission\Controllers\Permission::delete/$1');
-    $routes->post('permission/delete','Permission\Controllers\Permission::delete');
 
+$routes->group('admin', [
+    'namespace' => 'Admin',
+    'filter'    => 'login',
+], function ($routes) {
+    $routes->add('permission', 'Permission\Controllers\Permission::index');
+    $routes->post('permission/search', 'Permission\Controllers\Permission::search');
+
+    $permissionRoutes = [
+        'add'    => 'Permission\Controllers\Permission::add',
+        'edit/{segment}' => 'Permission\Controllers\Permission::edit/$1',
+        'delete/{segment}' => 'Permission\Controllers\Permission::delete/$1',
+    ];
+
+    foreach ($permissionRoutes as $method => $route) {
+        $routes->match(['get', 'post'], "permission/$method", $route);
+    }
+
+    $routes->post('permission/delete', 'Permission\Controllers\Permission::delete');
 });
