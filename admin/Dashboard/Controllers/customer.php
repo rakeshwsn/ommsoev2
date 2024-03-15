95f1487ce85285202375ae1057aa509bc794e260
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:05 +0530

    improve

--- admin/Dashboard/Controllers/customer.php
+++ admin/Dashboard/Controllers/customer.php
@@ -1,124 +1,144 @@
 <?php
+
 class ControllerExtensionDashboardCustomer extends Controller {
-	private $error = array();
 
-	public function index() {
-		$this->load->language('extension/dashboard/customer');
+    /**
+     * @var array
+     */
+    private $error = [];
 
-		$this->document->setTitle($this->language->get('heading_title'));
+    /**
+     * Index action for the dashboard customer page.
+     */
+    public function index() {
+        $this->load->language('extension/dashboard/customer');
 
-		$this->load->model('setting/setting');
+        $this->document->setTitle($this->language->get('heading_title'));
 
-		if (($this->request->server['REQUEST_METHOD'] == 'POST') && $this->validate()) {
-			$this->model_setting_setting->editSetting('dashboard_customer', $this->request->post);
+        $this->load->model('setting/setting');
 
-			$this->session->data['success'] = $this->language->get('text_success');
+        if ($this->request->server['REQUEST_METHOD'] == 'POST' && $this->validate()) {
+            $this->model_setting_setting->editSetting('dashboard_customer', $this->request->post);
 
-			$this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=dashboard', true));
-		}
+            $this->session->data['success'] = $this->language->get('text_success');
 
-		if (isset($this->error['warning'])) {
-			$data['error_warning'] = $this->error['warning'];
-		} else {
-			$data['error_warning'] = '';
-		}
+            $this->response->redirect($this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=dashboard', true));
+        }
 
-		$data['breadcrumbs'] = array();
+        $this->loadErrorMessages();
 
-		$data['breadcrumbs'][] = array(
-			'text' => $this->language->get('text_home'),
-			'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
-		);
+        $data['breadcrumbs'] = [
+            [
+                'text' => $this->language->get('text_home'),
+                'href' => $this->url->link('common/dashboard', 'user_token=' . $this->session->data['user_token'], true)
+            ],
+            [
+                'text' => $this->language->get('text_extension'),
+                'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=dashboard', true)
+            ],
+            [
+                'text' => $this->language->get('heading_title'),
+                'href' => $this->url->link('extension/dashboard/customer', 'user_token=' . $this->session->data['user_token'], true)
+            ],
+        ];
 
-		$data['breadcrumbs'][] = array(
-			'text' => $this->language->get('text_extension'),
-			'href' => $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=dashboard', true)
-		);
+        $data['action'] = $this->url->link('extension/dashboard/customer', 'user_token=' . $this->session->data['user_token'], true);
+        $data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=dashboard', true);
 
-		$data['breadcrumbs'][] = array(
-			'text' => $this->language->get('heading_title'),
-			'href' => $this->url->link('extension/dashboard/customer', 'user_token=' . $this->session->data['user_token'], true)
-		);
+        $this->load->model('localisation/language');
+        $data['languages'] = $this->model_localisation_language->getLanguages();
 
-		$data['action'] = $this->url->link('extension/dashboard/customer', 'user_token=' . $this->session->data['user_token'], true);
+        $data['columns'] = [];
+        for ($i = 3; $i <= 12; $i++) {
+            $data['columns'][] = $i;
+        }
 
-		$data['cancel'] = $this->url->link('marketplace/extension', 'user_token=' . $this->session->data['user_token'] . '&type=dashboard', true);
+        $this->loadSettings($data);
 
-		if (isset($this->request->post['dashboard_customer_width'])) {
-			$data['dashboard_customer_width'] = $this->request->post['dashboard_customer_width'];
-		} else {
-			$data['dashboard_customer_width'] = $this->config->get('dashboard_customer_width');
-		}
+        $data['header'] = $this->load->controller('common/header');
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['footer'] = $this->load->controller('common/footer');
 
-		$data['columns'] = array();
-		
-		for ($i = 3; $i <= 12; $i++) {
-			$data['columns'][] = $i;
-		}
-				
-		if (isset($this->request->post['dashboard_customer_status'])) {
-			$data['dashboard_customer_status'] = $this->request->post['dashboard_customer_status'];
-		} else {
-			$data['dashboard_customer_status'] = $this->config->get('dashboard_customer_status');
-		}
+        $this->response->setOutput($this->load->view('extension/dashboard/customer_form', $data));
+    }
 
-		if (isset($this->request->post['dashboard_customer_sort_order'])) {
-			$data['dashboard_customer_sort_order'] = $this->request->post['dashboard_customer_sort_order'];
-		} else {
-			$data['dashboard_customer_sort_order'] = $this->config->get('dashboard_customer_sort_order');
-		}
+    /**
+     * Load error messages if any.
+     */
+    private function loadErrorMessages() {
+        if (isset($this->error['warning'])) {
+            $data['error_warning'] = $this->error['warning'];
+        } else {
+            $data['error_warning'] = '';
+        }
+    }
 
-		$data['header'] = $this->load->controller('common/header');
-		$data['column_left'] = $this->load->controller('common/column_left');
-		$data['footer'] = $this->load->controller('common/footer');
+    /**
+     * Load settings for the form.
+     *
+     * @param array $data
+     */
+    private function loadSettings(&$data) {
+        if (isset($this->request->post['dashboard_customer_width'])) {
+            $data['dashboard_customer_width'] = $this->request->post['dashboard_customer_width'];
+        } else {
+            $data['dashboard_customer_width'] = $this->config->get('dashboard_customer_width');
+        }
 
-		$this->response->setOutput($this->load->view('extension/dashboard/customer_form', $data));
-	}
+        if (isset($this->request->post['dashboard_customer_status'])) {
+            $data['dashboard_customer_status'] = $this->request->post['dashboard_customer_status'];
+        } else {
+            $data['dashboard_customer_status'] = $this->config->get('dashboard_customer_status');
+        }
 
-	protected function validate() {
-		if (!$this->user->hasPermission('modify', 'extension/dashboard/customer')) {
-			$this->error['warning'] = $this->language->get('error_permission');
-		}
+        if (isset($this->request->post['dashboard_customer_sort_order'])) {
+            $data['dashboard_customer_sort_order'] = $this->request->post['dashboard_customer_sort_order'];
+        } else {
+            $data['dashboard_customer_sort_order'] = $this->config->get('dashboard_customer_sort_order');
+        }
+    }
 
-		return !$this->error;
-	}
-		
-	public function dashboard() {
-		$this->load->language('extension/dashboard/customer');
+    /**
+     * Validate the user's permission.
+     *
+     * @return bool
+     */
+    protected function validate() {
+        if (!$this->user->hasPermission('modify', 'extension/dashboard/customer')) {
+            $this->error['warning'] = $this->language->get('error_permission');
+        }
 
-		$data['user_token'] = $this->session->data['user_token'];
+        return !$this->error;
+    }
 
-		// Total Orders
-		$this->load->model('customer/customer');
+    /**
+     * Dashboard action for displaying customer information.
+     */
+    public function dashboard() {
+        $this->load->language('extension/dashboard/customer');
 
-		$today = $this->model_customer_customer->getTotalCustomers(array('filter_date_added' => date('Y-m-d', strtotime('-1 day'))));
+        $data['user_token'] = $this->session->data['user_token'];
 
-		$yesterday = $this->model_customer_customer->getTotalCustomers(array('filter_date_added' => date('Y-m-d', strtotime('-2 day'))));
+        $this->load->model('customer/customer');
 
-		$difference = $today - $yesterday;
+        $today = $this->model_customer_customer->getTotalCustomers(
+            [
+                'filter_date_added' => date('Y-m-d', strtotime('-1 day')),
+            ]
+        );
 
-		if ($difference && $today) {
-			$data['percentage'] = round(($difference / $today) * 100);
-		} else {
-			$data['percentage'] = 0;
-		}
+        $yesterday = $this->model_customer_customer->getTotalCustomers(
+            [
+                'filter_date_added' => date('Y-m-d', strtotime('-2 day')),
+            ]
+        );
 
-		$customer_total = $this->model_customer_customer->getTotalCustomers();
+        $difference = $today - $yesterday;
 
-		if ($customer_total > 1000000000000) {
-			$data['total'] = round($customer_total / 1000000000000, 1) . 'T';
-		} elseif ($customer_total > 1000000000) {
-			$data['total'] = round($customer_total / 1000000000, 1) . 'B';
-		} elseif ($customer_total > 1000000) {
-			$data['total'] = round($customer_total / 1000000, 1) . 'M';
-		} elseif ($customer_total > 1000) {
-			$data['total'] = round($customer_total / 1000, 1) . 'K';
-		} else {
-			$data['total'] = $customer_total;
-		}
+        if ($difference && $today) {
+            $data['percentage'] = round(($difference / $today) * 100);
+        } else {
+            $data['percentage'] = 0;
+        }
 
-		$data['customer'] = $this->url->link('customer/customer', 'user_token=' . $this->session->data['user_token'], true);
-
-		return $this->load->view('extension/dashboard/customer_info', $data);
-	}
-}
+        $customer_
