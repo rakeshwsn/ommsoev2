15da55c1499fe198e6d5d67466d51195f4122922
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:57 +0530

    improve

--- admin/Reports/Views/index.php
+++ admin/Reports/Views/index.php
@@ -1,3 +1,7 @@
 <section>
-    <h1>Module Reports => Index</h1>
+    <h1>Module Reports</h1>
+    <p>Welcome to the module reports page. From here, you can access all the reports related to the modules in our system.</p>
+    <ul>
+        <li><a href="/reports/modules">View all module reports</a></li>
+    </ul>
 </section>
