6695790da533ff833bbcaf581e7d454c8663cb3f
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:18 +0530

    improve

--- admin/Users/Views/allow_upload.php
+++ admin/Users/Views/allow_upload.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Allow Upload</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker1/1.9.0/css/bootstrap-datepicker.min.css">
+</head>
+<body>
 
 <div class="col-12">
     <div class="block block-themed">
@@ -11,7 +21,7 @@
                         <select class="form-control" id="year" name="year">
                             <option value="">Choose Year</option>
                             <?php foreach (getAllYears() as $_year) { ?>
-                                <option value="<?=$_year['id']?>" <?php if ($_year['id']==$year_id){ echo 'selected'; } ?>><?=$_year['name']?></option>
+                                <option value="<?=htmlspecialchars($_year['id'])?>" <?php if ($_year['id']==$year_id){ echo 'selected'; } ?>><?=htmlspecialchars($_year['name'])?></option>
                             <?php } ?>
                         </select>
                     </div>
@@ -19,7 +29,7 @@
                         <select class="form-control" id="month" name="month">
                             <option value="">Choose Month</option>
                             <?php foreach (getAllMonths() as $_month) { ?>
-                                <option value="<?=$_month['id']?>" <?php if ($_month['id']==$month_id){ echo 'selected'; } ?>><?=$_month['name']?></option>
+                                <option value="<?=htmlspecialchars($_month['id'])?>" <?php if ($_month['id']==$month_id){ echo 'selected'; } ?>><?=htmlspecialchars($_month['name'])?></option>
                             <?php } ?>
                         </select>
                     </div>
@@ -27,7 +37,7 @@
                         <select class="form-control" id="agency_type_id" name="agency_type_id">
                             <option value="">Choose Agency</option>
                             <?php foreach ($agency_types as $_agency) { ?>
-                                <option value="<?=$_agency['id']?>" <?php if ($_agency['id']==$agency_type_id){ echo 'selected'; } ?>><?=$_agency['name']?></option>
+                                <option value="<?=htmlspecialchars($_agency['id'])?>" <?php if ($_agency['id']==$agency_type_id){ echo 'selected'; } ?>><?=htmlspecialchars($_agency['name'])?></option>
                             <?php } ?>
                         </select>
                     </div>
@@ -36,13 +46,13 @@
                             <select class="form-control" id="district_id" name="district_id">
                                 <option value="">Choose District</option>
                                 <?php foreach ($districts as $_district) { ?>
-                                    <option value="<?=$_district['id']?>" <?php if ($_district['id']==$district_id){ echo 'selected'; } ?>><?=$_district['name']?></option>
+                                    <option value="<?=htmlspecialchars($_district['id'])?>" <?php if ($_district['id']==$district_id){ echo 'selected'; } ?>><?=htmlspecialchars($_district['name'])?></option>
                                 <?php } ?>
                             </select>
                         </div>
                     <?php endif; ?>
                     <div class="col-md-2">
-                        <button class="btn btn-primary"><i class="si si-magnifier"></i> Submit</button>
+                        <button class="btn btn-primary"><i class="fa fa-search"></i> Submit</button>
                     </div>
                 </div>
             </form>
@@ -64,17 +74,17 @@
                 <tbody>
                 <?php foreach ($users as $user): ?>
                     <tr>
-                        <td><?=$user['district']?></td>
-                        <td><?=$user['block']?></td>
-                        <td><?=$user['firstname']?></td>
-                        <td><?=$user['from_date']?></td>
-                        <td><?=$user['to_date']?></td>
+                        <td><?=htmlspecialchars($user['district'])?></td>
+                        <td><?=htmlspecialchars($user['block'])?></td>
+                        <td><?=htmlspecialchars($user['firstname'])?></td>
+                        <td><?=htmlspecialchars($user['from_date'])?></td>
+                        <td><?=htmlspecialchars($user['to_date'])?></td>
                         <td><input type="text"
-                                   data-upload-id="<?=$user['upload_id']?>"
-                                   data-user-id="<?=$user['user_id']?>"
+                                   data-upload-id="<?=htmlspecialchars($user['upload_id'])?>"
+                                   data-user-id="<?=htmlspecialchars($user['user_id'])?>"
                                    data-date-format="dd/mm/yyyy"
                                    class="js-datepicker form-control"
-                                   value="<?=$user['extended_date']?>"></td>
+                                   value="<?=htmlspecialchars($user['extended_date'])?>"></td>
                     </tr>
                 <?php endforeach; ?>
                 </tbody>
@@ -83,41 +93,4 @@
     </div>
 </div>
 
-<?php js_start(); ?>
-<script>
-    $(function () {
-        // Init datepicker (with .js-datepicker and .input-daterange class)
-        $('.js-datepicker:not(.js-datepicker-enabled)').add('.input-daterange:not(.js-datepicker-enabled)').each(function(){
-            var el = $(this);
-
-            // Add .js-datepicker-enabled class to tag it as activated
-            el.addClass('js-datepicker-enabled');
-
-            // Init
-            el.datepicker({
-                weekStart: el.data('week-start') || 0,
-                autoclose: true,
-                todayHighlight: true,
-                orientation: 'bottom', // Position issue when using BS4, set it to bottom until officially supported
-            }).on('changeDate', function (e) {
-                upload_id = $(this).data('upload-id');
-                user_id = $(this).data('user-id');
-                to_date = $(this).val();
-                $.ajax({
-                    url:'<?=$allow_upload_url?>',
-                    data:{upload_id:upload_id,user_id:user_id,to_date:to_date},
-                    type:'POST',
-                    dataType:'JSON',
-                    success:function (json) {
-                        alert('Date extended successfully');
-                    },
-                    error:function () {
-                        alert('Unable to update date');
-                    }
-                });
-            });
-        });
-
-    });
-</script>
-<?php js_end(); ?>
+<script src="https://cdnjs.
