067afaaf69a94cf8b3e20b9aa99fd81fcca19cb4
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:07 +0530

    improve

--- admin/MIS/Views/index.php
+++ admin/MIS/Views/index.php
@@ -1,46 +1,40 @@
-
-<!-- Main content -->
-
 <h2 class="content-heading">MIS</h2>
-<?php if ($upload_enabled) { ?>
+
+<form action="your_action_url_here" method="post" novalidate>
     <div class="block" id="upload-controls">
         <div class="block-content block-content-full">
             <div class="row">
                 <div class="col-md-2">
-                    <select class="form-control" id="year" name="year" required>
-                        <option value="">Choose Year</option>
+                    <label for="year">Choose Year</label>
+                    <select class="form-control" id="year" name="year">
+                        <option value="">Select</option>
                         <?php foreach ($years as $year) { ?>
-                            <option value="<?= $year['id'] ?>" <?php if ($year['id'] == $year_id) {
-                                echo 'selected';
-                            } ?>><?= $year['name'] ?></option>
+                            <option value="<?= $year['id'] ?>"><?= $year['name'] ?></option>
                         <?php } ?>
                     </select>
                 </div>
                 <div class="col-md-2">
-                    <select class="form-control" id="month" name="month" required>
-                        <option value="">Choose Month</option>
+                    <label for="month">Choose Month</label>
+                    <select class="form-control" id="month" name="month">
+                        <option value="">Select</option>
                         <?php foreach ($months as $month) { ?>
-                            <option value="<?= $month['id'] ?>" <?php if ($month['id'] == $month_id) {
-                                echo 'selected';
-                            } ?>><?= $month['name'] ?></option>
+                            <option value="<?= $month['id'] ?>"><?= $month['name'] ?></option>
                         <?php } ?>
                     </select>
                 </div>
             </div>
             <div class="row mt-3">
                 <div class="col-md-2">
-                    <button id="btn-add" class="btn btn-outline btn-primary"><i class="fa fa-table"></i> Add New
-                    </button>
+                    <button type="button" id="btn-add" class="btn btn-outline btn-primary"><i class="fa fa-table" aria-hidden="true" title="Add New"></i> Add New</button>
                 </div>
             </div>
         </div>
     </div>
-<?php } ?>
+</form>
 
 <div class="block">
-
     <div class="block-content block-content-full">
-        <table class="table table-bordered table-striped table-vcenter js-dataTable-full" id="datatable">
+        <table class="table table-bordered table-striped table-vcenter js-dataTable-full" id="datatable" style="display: none;">
             <thead>
             <tr>
                 <th>ID</th>
@@ -51,73 +45,58 @@
                 <th>Action</th>
             </tr>
             </thead>
+            <tbody>
+            </tbody>
         </table>
     </div>
 </div>
-<!-- content -->
-<?php js_start(); ?>
-<script>
 
-    var loading;
-
+<script type="text/javascript" defer>
     $(function () {
         $('#datatable').dataTable({
-            "processing": true,
-            "serverSide": true,
-            "responsive": false,
-            "filter":false,
-            "columnDefs": [
-                { targets: [3,4], orderable: false },
+            processing: true,
+            serverSide: true,
+            responsive: false,
+            filter: false,
+            columnDefs: [
+                { targets: [3, 4], orderable: false },
                 { targets: [0], visible: false },
             ],
-            'order':[0,'desc'],
-            "ajax":{
-                headers: {'X-Requested-With': 'XMLHttpRequest'},
-                url :"<?=$datatable_url?>", // json datasource
-                type: "post",  // method  , by default get
-                dataType:'json',
-                beforeSend:function () {
-//                    $('#main-container').loading();
+            order: [0, 'desc'],
+            ajax: {
+                headers: { 'X-Requested-With': 'XMLHttpRequest' },
+                url: "your_datatable_url_here",
+                type: "post",
+                dataType: 'json',
+                beforeSend: function () {
                     $("#main-container").LoadingOverlay("show");
                 },
-                error: function(){  // error handling
+                error: function () {
                     $(".datatable-error").html("");
-                    $("#datatable").append('<tbody class="datatable-error"><tr><th colspan="3">No data found.</th></tr></tbody>');
-                    $("#datatable_processing").css("display","none");
-
+                    $("#datatable tbody").html('<tr><th colspan="3">No data found.</th></tr>');
+                    $("#datatable_processing").css("display", "none");
                 },
-                complete:function () {
+                complete: function () {
                     $("#main-container").LoadingOverlay("hide");
                 }
             },
         });
 
-        $(document).on('click','.btn-delete',function (e) {
-            if(confirm('Are you sure, you want to delete this record?')===false){
+        $(document).on('click', '.btn-delete', function (e) {
+            if (confirm('Are you sure, you want to delete this record?') === false) {
                 e.preventDefault();
             }
         });
     });
 
-    //headers: {'X-Requested-With': 'XMLHttpRequest'}
-
-    var add_url = '<?=$add_url?>';
+    var add_url = 'your_add_url_here';
 
     $('#btn-add').click(function (e) {
         e.preventDefault();
         setLocation(add_url);
     });
+
     function setLocation(url) {
         var _year = $('#year').val();
         var month = $('#month').val();
         var block_id = $('#block_id').val() || '';
-        var district_id = $('#district_id').val() || '';
-        var agency_type_id = $('#agency_type_id').val() || '';
-        var fund_agency_id = $('#fund_agency_id').val() || '';
-        location.href = url+'?month='+month+'&year='+_year+'&block_id='+block_id+'&district_id='+district_id
-            +'&agency_type_id='+agency_type_id+'&fund_agency_id='+fund_agency_id;
-    }
-
-
-</script>
-<?php js_end(); ?>
