ef25b2cae25705d3edcd1c98806b73f62c8d11d0
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:25 +0530

    improve

--- front/Pages/Views/page.php
+++ front/Pages/Views/page.php
@@ -1,26 +1,43 @@
-<?php echo $content; ?>
-<?php js_start(); ?>
-<script type="text/javascript"><!--
-$(function(){
-	$("#complain_form").submit(function(e) {
-	//alert("ok");
-    e.preventDefault(); // avoid to execute the actual submit of the form.
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Improved Complain Form</title>
+    <?php js_start(); ?>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <?php js_end(); ?>
+</head>
+<body>
+    <div id="content">
+        <?php echo $content; ?>
+    </div>
 
-    var form = $(this);
-    var actionUrl = form.attr('action');
-    
-    $.ajax({
-        type: "POST",
-        url: actionUrl,
-        data: form.serialize(), // serializes the form's elements.
-        success: function(data)
-        {
-          alert(data); // show response from the php script.
-        }
+    <form id="complain_form" action="complain_action.php" method="post">
+        <!-- Add your form fields here -->
+    </form>
+
+    <?php js_start(); ?>
+    <script type="text/javascript">
+    $(document).ready(function(){
+        $('#complain_form').on('submit', function(e) {
+            e.preventDefault();
+
+            var form = $(this);
+            var actionUrl = form.attr('action');
+
+            $.ajax({
+                type: "POST",
+                url: actionUrl,
+                data: form.serialize(),
+                success: function(data) {
+                    alert(data);
+                },
+                error: function(xhr, status, error) {
+                    console.log(xhr.responseText);
+                }
+            });
+        });
     });
-    
-});
-});
-
-//--></script>
-<?php js_end(); ?>
+    </script>
+    <?php js_end(); ?>
+</body>
+</html>
