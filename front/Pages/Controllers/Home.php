d2f89b46ecd795773beb9e16890e06f0ede5a739
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:25 +0530

    improve

--- front/Pages/Controllers/Home.php
+++ front/Pages/Controllers/Home.php
@@ -36,34 +36,41 @@ class Home extends BaseController
     {
         $data = [];
 
-        $page = $this->pageModel->find(1) ?? [];
+        $page = $this->pageModel->find(1);
 
-        if (!empty($page)) {
-            if ($page['status'] !== 'published') {
-                if ($page['status'] !== 'draft') {
-                    throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound();
-                }
-            }
-
-            $this->template->set_meta_title($page['title']);
-            $data['content'] = html_entity_decode($page['content'], ENT_QUOTES, 'UTF-8');
-            $this->template->set('site_name', trim($this->settings->config_site_title));
+        if (empty($page)) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException();
         }
+
+        if ($page['status'] !== 'published' && $page['status'] !== 'draft') {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException();
+        }
+
+        $this->template->set_meta_title($page['title']);
+        $data['content'] = html_entity_decode($page['content'], ENT_QUOTES, 'UTF-8');
+        $this->template->set('site_name', trim($this->settings->config_site_title));
+
+        $banners = $this->bannerModel->getBannerImages(3);
+        $sliders = $this->bannerModel->getBannerImages(4);
 
         $data['banners'] = array_map(fn ($banner) => [
             'image' => upload_url($banner['image']),
             'title' => $banner['title'],
             'link' => $banner['link'],
             'description' => $banner['description'],
-        ], $this->bannerModel->getBannerImages(3) ?? []);
+        ], $banners ?? []);
 
         $data['sliders'] = array_map(fn ($slider) => [
             'image' => upload_url($slider['image']),
             'title' => $slider['title'],
             'link' => $slider['link'],
             'description' => $slider['description'],
-        ], $this->bannerModel->getBannerImages(4) ?? []);
+        ], $sliders ?? []);
 
         $this->template->add_package(['lightgallery'], true);
         $this->template->add_javascript('themes/default/assets/js/plugins.js');
-        $this->template
+
+        // Make sure to return the final view
+        return $this->template->render('home', $data);
+    }
+}
