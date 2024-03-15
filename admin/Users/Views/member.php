f841ed16d1d85860a0fae06c71d8a1f75232d42f
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:20 +0530

    improve

--- admin/Users/Views/member.php
+++ admin/Users/Views/member.php
@@ -2,24 +2,30 @@
 	<div class="block-header block-header-default">
 		<h3 class="block-title"><?php echo $heading_title; ?></h3>
 		<div class="block-options">
-			<a href="<?php echo $add; ?>" data-toggle="tooltip" title="<?php echo $button_add; ?>" class="btn btn-primary"><i class="fa fa-plus"></i></a>
-			<button type="button" data-toggle="tooltip" title="<?php echo $button_delete; ?>" class="btn btn-danger" onclick="confirm('<?php echo $text_confirm; ?>') ? $('#form-member').submit() : false;"><i class="fa fa-trash-o"></i></button>
+			<label class="btn btn-primary" for="select_all">
+				<input type="checkbox" id="select_all" onclick="$('input[name*=\'selected\']').prop('checked', this.checked);" />
+				<i class="fa fa-plus"></i> <?php echo $button_add; ?>
+			</label>
+			<button type="button" data-toggle="tooltip" title="<?php echo $button_delete; ?>" class="btn btn-danger" onclick="confirm('<?php echo $text_confirm; ?>') ? $('#form-member').submit() : false;">
+				<i class="fa fa-trash-o"></i>
+			</button>
 		</div>
 	</div>
 	<div class="block-content block-content-full">
-		<!-- DataTables functionality is initialized with .js-dataTable-full class in js/member/be_tables_datatables.min.js which was auto compiled from _es6/member/be_tables_datatables.js -->
-		<form action="<?php echo $delete; ?>" method="post" enctype="multipart/form-data" id="form-member">        		
-		<table id="member_list" class="table table-bordered table-striped table-vcenter js-dataTable-full">
-			<thead>
-				<tr>
-					<th style="width: 1px;" class="text-center no-sort"><input type="checkbox" onclick="$('input[name*=\'selected\']').prop('checked', this.checked);" /></th>
-					<th>Name</th>
-                    <th>Designation</th>
-					<th>Status</th>
-					<th class="text-right no-sort">Actions</th>
-				</tr>
-			</thead>
-		</table>
+		<form action="<?php echo $delete_url; ?>" method="post" id="form-member">
+			<table id="member_list" class="table table-bordered table-striped table-vcenter js-dataTable-full">
+				<thead>
+					<tr>
+						<th style="width: 1px;" class="text-center no-sort"><input type="checkbox" name="selected[]" /></th>
+						<th>Name</th>
+						<th>Designation</th>
+						<th>Status</th>
+						<th class="text-right no-sort">Actions</th>
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
 		</form>
 	</div>
 </div>
@@ -34,16 +40,4 @@ $(function(){
 		],
 		"ajax":{
 			url :"<?=$datatable_url?>", // json datasource
-			type: "post",  // method  , by default get
-			error: function(){  // error handling
-				$(".member_list_error").html("");
-				$("#member_list").append('<tbody class="member_list_error"><tr><th colspan="5">No data found.</th></tr></tbody>');
-				$("#member_list_processing").css("display","none");
-				
-			},
-			dataType:'json'
-		},
-	});
-});
-//--></script>
-<?php js_end(); ?>
+			
