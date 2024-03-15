d72bd18e19dd83a82a7071fa08e6d09f8ce665af
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:11 +0530

    improve

--- admin/Incentive/Config/Routes.php
+++ admin/Incentive/Config/Routes.php
@@ -1,27 +1,34 @@
 <?php
-namespace Admin\Incentive\Config;
-if(!isset($routes))
-{ 
-    $routes = \Config\Services::routes(true);
-}
 
-$routes->group('admin', ['namespace' => 'Admin','filter' => 'login'], function($routes)
+namespace App\Config;
+
+use Config\Services;
+
+class Routes
 {
-    $routes->add('incentive', 'Incentive\Controllers\Incentive::index');
-    $routes->post('incentive/search','Incentive\Controllers\Incentive::search');
-    $routes->post('incentive/searchmain','Incentive\Controllers\Incentive::searchMain');
-    $routes->match(['get','post'],'incentive/view/(:segment)', 'Incentive\Controllers\Incentive::view/$1');
-    $routes->match(['get','post'],'incentive/add', 'Incentive\Controllers\Incentive::add');
-    $routes->match(['get','post'],'incentive/addform', 'Incentive\Controllers\Incentive::addform');
-    $routes->match(['get','post'],'incentive/edit/(:segment)', 'Incentive\Controllers\Incentive::edit/$1');
-    $routes->get('incentivemain/delete/(:segment)',   'Incentive\Controllers\Incentive::delete/$1');
-    $routes->post('incentive/delete','Incentive\Controllers\Incentive::delete');
-    // $routes->add('incentive/incentivesearch/(:segment)', 'Incentive\Controllers\Incentive::incentivesearch/$1');
-    $routes->add('incentive/incentivesearch', 'Incentive\Controllers\Incentive::incentivesearch');
-    $routes->post('incentive/searchall','Incentive\Controllers\Incentive::searchall');
-    $routes->post('incentive/ajaxverify','Incentive\Controllers\Incentive::ajaxverify');
-    $routes->post('incentive/ajaxverifyupdate','Incentive\Controllers\Incentive::ajaxverifyupdate');
-    $routes->get('incentive/downloadexcel','Incentive\Controllers\Incentive::downloadExcelFile');
-    $routes->get('incentive/countExcelHeaders','Incentive\Controllers\Incentive::countExcelHeaders');
+    public static function incentive()
+    {
+        $routes = Services::routes(true);
 
-});
+        $routes->group('admin', [
+            'namespace' => 'Admin',
+            'filter' => 'login',
+        ], function ($routes) {
+            $routes->add('incentive', 'Incentive\Controllers\Incentive::index');
+            $routes->post('incentive/search', 'Incentive\Controllers\Incentive::search');
+            $routes->post('incentive/searchmain', 'Incentive\Controllers\Incentive::searchMain');
+            $routes->match(['get', 'post'], 'incentive/view/(:segment)', 'Incentive\Controllers\Incentive::view/$1');
+            $routes->match(['get', 'post'], 'incentive/add', 'Incentive\Controllers\Incentive::add');
+            $routes->match(['get', 'post'], 'incentive/addform', 'Incentive\Controllers\Incentive::addform');
+            $routes->match(['get', 'post'], 'incentive/edit/(:segment)', 'Incentive\Controllers\Incentive::edit/$1');
+            $routes->get('incentivemain/delete/(:segment)', 'Incentive\Controllers\Incentive::delete/$1');
+            $routes->post('incentive/delete', 'Incentive\Controllers\Incentive::delete');
+            $routes->add('incentive/incentivesearch', 'Incentive\Controllers\Incentive::incentivesearch');
+            $routes->post('incentive/searchall', 'Incentive\Controllers\Incentive::searchall');
+            $routes->post('incentive/ajaxverify', 'Incentive\Controllers\Incentive::ajaxverify');
+            $routes->post('incentive/ajaxverifyupdate', 'Incentive\Controllers\Incentive::ajaxverifyupdate');
+            $routes->get('incentive/downloadexcel', 'Incentive\Controllers\Incentive::downloadExcelFile');
+            $routes->get('incentive/countExcelHeaders', 'Incentive\Controllers\Incentive::countExcelHeaders');
+        });
+    }
+}
