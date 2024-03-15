b9b7eb17a9b604d3c4803857b52ed71fbb7cdd44
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:09 +0530

    improve

--- front/Banner/Controllers/Banner.php
+++ front/Banner/Controllers/Banner.php
@@ -1,48 +1,63 @@
 <?php
+
 namespace Front\Banner\Controllers;
+
 use Admin\Banner\Models\BannerModel;
 use App\Controllers\BaseController;
 
 class Banner extends BaseController
 {
-	private $error = array();
+    /**
+     * @var BannerModel
+     */
     private $bannerModel;
-    function __construct(){
-		$this->bannerModel = new BannerModel();
-	}
 
-	public function getGallery($attr){
-        $this->template->add_package(['lightgallery'],true);
-        extract($attr);
-        $data['sliders'] = [];
-        foreach ($this->bannerModel->getBannerImages($id) as $slider) {
+    public function __construct()
+    {
+        $this->bannerModel = new BannerModel();
+    }
+
+    /**
+     * @param array $attr
+     * @return string
+     */
+    public function getGallery(array $attr): string
+    {
+        $this->template->addPackage(['lightgallery'], true);
+        $data = $this->prepareBannerData($this->bannerModel->getBannerImages($attr['id'] ?? 0), 'gallery');
+
+        return $this->template->view('Front\Banner\Views\gallery', $data, true);
+    }
+
+    /**
+     * @param array $attr
+     * @return string
+     */
+    public function getSlider(array $attr): string
+    {
+        $data = $this->prepareBannerData($this->bannerModel->getBannerImages(3), 'slider');
+
+        return $this->template->view('Front\Banner\Views\slider', $data, true);
+    }
+
+    /**
+     * @param array $bannerImages
+     * @param string $templateName
+     * @return array
+     */
+    private function prepareBannerData(array $bannerImages, string $templateName): array
+    {
+        $data = ['sliders' => []];
+
+        foreach ($bannerImages as $bannerImage) {
             $data['sliders'][] = [
-                'image' => upload_url($slider['image']),
-                'title' => $slider['title'],
-                'link' => $slider['link'],
-                'description' => $slider['description'],
+                'image' => upload_url($bannerImage['image']),
+                'title' => $bannerImage['title'],
+                'link' => $bannerImage['link'],
+                'description' => $bannerImage['description'],
             ];
         }
-        //printr($data['sliders']);
-        return $this->template->view('Front\Banner\Views\gallery',$data,true);
 
+        return $data;
     }
-
-    public function getSlider($attr){
-        extract($attr);
-        $data['banners'] = [];
-
-        foreach ($this->bannerModel->getBannerImages(3) as $banner) {
-            $data['banners'][] = [
-                'image' => upload_url($banner['image']),
-                'title' => $banner['title'],
-                'link' => $banner['link'],
-                'description' => $banner['description'],
-            ];
-        }
-        //printr($data['sliders']);
-        return $this->template->view('Front\Banner\Views\slider',$data,true);
-
-    }
-
 }
