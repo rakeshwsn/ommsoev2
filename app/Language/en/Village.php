e2c30b7800df8f569b66807b3fc407983c65e7eb
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:21 +0530

    improve

--- app/Language/en/Village.php
+++ app/Language/en/Village.php
@@ -1,15 +1,48 @@
 <?php
-// An array to store text strings for the Village module
-$text = [
-    'heading_title'        	=> 'Village', // The title of the page
-    'text_list'           	=> 'Village List', // The title of the list page
-    'text_edit'            	=> 'Edit Village', // The title of the edit page
-    'text_add'            	=> 'Add Village' // The title of the add page
-];
 
-// A function to return the text array
-function getText() {
-    global $text;
-    return $text;
+namespace App\Modules\Village;
+
+/**
+ * Class Text
+ *
+ * Provides text strings for the Village module
+ */
+class Text
+{
+    /**
+     * The title of the page
+     * @var string
+     */
+    public const HEADING_TITLE = 'Village';
+
+    /**
+     * The title of the list page
+     * @var string
+     */
+    public const TEXT_LIST = 'Village List';
+
+    /**
+     * The title of the edit page
+     * @var string
+     */
+    public const TEXT_EDIT = 'Edit Village';
+
+    /**
+     * The title of the add page
+     * @var string
+     */
+    public const TEXT_ADD = 'Add Village';
+
+    /**
+     * @return array<string, string>
+     */
+    public static function getText(): array
+    {
+        return [
+            'heading_title' => self::HEADING_TITLE,
+            'text_list' => self::TEXT_LIST,
+            'text_edit' => self::TEXT_EDIT,
+            'text_add' => self::TEXT_ADD,
+        ];
+    }
 }
-?>
