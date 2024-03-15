6a9b0e83039ba51cce4493e2fbb9957658c5898a
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:02 +0530

    improve

--- admin/FPO/Views/gform.php
+++ admin/FPO/Views/gform.php
@@ -1,67 +1,60 @@
-<form method="post" action="<?=$action?>" id="fpo-form-details" onsubmit="return false;" enctype="multipart/form-data">
-    <?php
-    $html="";
-    foreach($formdata as $form){
-        switch($form['input_type']){
-            case "text":
-               $html.=generateTextHtml($form);
-               break;
-            case "email":
-                $html.=generateEmailHtml($form);
-                break;
-            case "date":
-                $html.=generateDateHtml($form);
-                break;
-            case "select":
-                $html.=generateSelectHtml($form);
-                break;
-            case "file":
-                $html.=generateFileHtml($form);
-                break;
-            default:
-                $html.="";
-        }
+<?php
+error_reporting(E_ALL);
 
-  }
-  echo $html;
+$html = "";
+foreach ($formdata as $form) {
+    if (empty($form['input_type'])) {
+        continue;
+    }
+
+    switch (trim($form['input_type'])) {
+        case "text":
+            $html .= generateTextHtml($form);
+            break;
+        case "email":
+            $html .= generateEmailHtml($form);
+            break;
+        case "date":
+            $html .= generateDateHtml($form);
+            break;
+        case "select":
+            $html .= generateSelectHtml($form);
+            break;
+        case "file":
+            $html .= generateFileHtml($form);
+            break;
+        default:
+            $html .= "";
+    }
+}
+
+echo $html;
 ?>
-</form>
+
 <script>
-    jQuery(function(){
-        Codebase.helpers(['flatpickr']);
-    });
-    jQuery('[data-toggle="custom-file-input"]:not(.js-custom-file-input-enabled)').each(function (e, a) {
-        var t = jQuery(a);
-        t.addClass("js-custom-file-input-enabled").on("change", function (e) {
-            var a = e.target.files.length > 1 ? e.target.files.length + " " + (t.data("lang-files") || "Files") : e.target.files[0].name;
-            t.next(".custom-file-label").css("overflow-x", "hidden").html(a);
+jQuery(function ($) {
+    Codebase.helpers(['flatpickr']);
+
+    $('[data-toggle="custom-file-input"]:not(.js-custom-file-input-enabled)').each(function (e, a) {
+        var $t = $(a);
+        $t.addClass("js-custom-file-input-enabled").on("change", function (e) {
+            var a = e.target.files.length > 1 ? e.target.files.length + " " + ($t.data("lang-files") || "Files") : e.target.files[0].name;
+            $t.next(".custom-file-label").css("overflow-x", "hidden").html(a);
         });
     });
 
-    jQuery(function(){
-        $("[data-calculation*='+']").each( function() {
-            var cal=$(this).data('calculation');
-            var id=$(this).attr('id')
-            //"a=b,c:d".split('=').join(',').split(':').join(',').split(',')
-            const calarray = cal.split("+");
-            calarray.forEach(function (item,index){
-                $("#"+item).attr('data-targetid',id);
-            });
+    $("[data-calculation]").each(function () {
+        var $this = $(this);
+        var calculation = $this.data('calculation');
+        var targetId = $this.attr('id');
 
+        calculation.split('+').forEach(function (item) {
+            $("#" + item).attr('data-calculation-target', targetId);
         });
+    });
 
-        $(document).on('keyup', "[data-targetid]", function() {
-            var target=$(this).data('targetid');
+    $(document).on('keyup', "[data-calculation-target]", function () {
+        var targetId = $(this).data('calculation-target');
+        var sum = 0;
 
-            var sum=0;
-            $("[data-targetid]").each( function() {
-                console.log($(this).val());
-                sum+=parseInt($(this).val());
-            });
-            console.log(sum);
-            $("#"+target).val(sum);
-        });
-
-
-    })
-</script>
+        $("[data-calculation-target]").each(function
