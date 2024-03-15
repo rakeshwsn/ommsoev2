1d97bfe9f7328c2d77204f15af4fcd87b640dcdb
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:12 +0530

    improve

--- admin/UC/Models/UCSubmitModel.php
+++ admin/UC/Models/UCSubmitModel.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Admin\UC\Models;
 
 use Admin\Localisation\Models\BlockModel;
@@ -9,105 +10,113 @@ use CodeIgniter\Validation\ValidationInterface;
 
 class UCSubmitModel extends Model
 {
-	protected $DBGroup              = 'default';
-	protected $table                = 'soe_uc_submit';
-	protected $primaryKey           = 'id';
-	protected $useAutoIncrement     = true;
-	protected $insertID             = 0;
-	protected $returnType           = 'object';
-	protected $useSoftDeletes        = true;
-	protected $protectFields        = false;
-//	protected $allowedFields        = [];
+    protected $DBGroup              = 'default';
+    protected $table                = 'soe_uc_submit';
+    protected $primaryKey           = 'id';
+    protected $useAutoIncrement     = true;
+    protected $insertID             = 0;
+    protected $returnType           = 'object';
+    protected $useSoftDeletes        = true;
+    protected $protectFields        = false;
+    // protected $allowedFields        = [];
 
-	// Dates
-	protected $useTimestamps        = true;
-	protected $dateFormat           = 'datetime';
-	protected $createdField         = 'created_at';
-	protected $updatedField         = '';
-	protected $deletedField         = 'deleted_at';
+    // Dates
+    protected $useTimestamps        = true;
+    protected $dateFormat           = 'datetime';
+    protected $createdField         = 'created_at';
+    protected $updatedField         = '';
+    protected $deletedField         = 'deleted_at';
 
-    //add fund agency id (niranjan)
-    public function getRecipientId($district_id,$fund_agency_id=1) {
-        $sql = "SELECT id FROM soe_uc_recipients r WHERE r.district_id=".(int)$district_id ." AND r.fund_agency_id=".(int)$fund_agency_id;
+    public function __construct(ConnectionInterface &$db)
+    {
+        parent::__construct($db);
+    }
 
-        $res = $this->db->query($sql)->getFirstRow();
+    // Get recipient ID based on district ID and fund agency ID
+    public function getRecipientId(int $district_id, int $fund_agency_id = 1): ?int
+    {
+        $sql = "SELECT id FROM soe_uc_recipients r WHERE r.district_id = ? AND r.fund_agency_id = ?";
+        $res = $this->db->query($sql, [$district_id, $fund_agency_id])->getFirstRow();
 
-        if(!$res){
+        if (!$res) {
             return null;
-        } else {
-            return $res->id;
         }
-	}
 
-    public function getUCReport($filter=[]) {
+        return $res->id;
+    }
 
+    // Get UC report based on filter
+    public function getUCReport(array $filter = []): array
+    {
         $recipient_id = 0;
-        if(!empty($filter['recipient_id'])) {
+
+        if (isset($filter['recipient_id'])) {
             $recipient_id = $filter['recipient_id'];
         }
-        if(!empty($filter['district_id'])) {
+
+        if (isset($filter['district_id'])) {
             $res = $this->getRecipientId($filter['district_id']);
 
             if (!$res) {
                 return [];
-            } else {
-                $recipient_id = $res;
             }
+
+            $recipient_id = $res;
         }
 
         $sql = "SELECT
-  allt.recipient_id,
-  allt.year year_id,
-  y.name `year`,
-  allt.allotment,
-  sbmt.date_submit,
-  sbmt.letter_no,
-  sbmt.uc_submit,
-  (allt.allotment - sbmt.uc_submit) balance
-FROM (SELECT
-    sua.recipient_id,
-    sua.year,
-    SUM(sua.amount) allotment
-  FROM soe_uc_allotment sua
-  WHERE sua.deleted_at IS NULL
-  AND sua.recipient_id = ".$recipient_id."
-  GROUP BY sua.year) allt
-  LEFT JOIN (SELECT
-      sua.recipient_id,
-      sua.year,
-      MAX(sus.date_submit) date_submit,
-      GROUP_CONCAT(sus.letter_no SEPARATOR ', ') letter_no,
-      SUM(sus.amount) uc_submit
-    FROM soe_uc_submit sus
-      LEFT JOIN (SELECT
-          *
-        FROM soe_uc_allotment
-        WHERE recipient_id = ".$recipient_id.") sua
-        ON sus.allotment_id = sua.id
-    WHERE sua.deleted_at IS NULL
-    AND sus.deleted_at IS NULL
-    GROUP BY sua.year) sbmt
-    ON allt.year = sbmt.year LEFT JOIN soe_years y ON y.id=allt.year";
+            allt.recipient_id,
+            allt.year year_id,
+            y.name `year`,
+            allt.allotment,
+            sbmt.date_submit,
+            sbmt.letter_no,
+            sbmt.uc_submit,
+            (allt.allotment - sbmt.uc_submit) balance
+          FROM (SELECT
+                sua.recipient_id,
+                sua.year,
+                SUM(sua.amount) allotment
+              FROM soe_uc_allotment sua
+              WHERE sua.deleted_at IS NULL
+                AND sua.recipient_id = ?
+              GROUP BY sua.year) allt
+            LEFT JOIN (SELECT
+                        sua.recipient_id,
+                        sua.year,
+                        MAX(sus.date_submit) date_submit,
+                        GROUP_CONCAT(sus.letter_no SEPARATOR ', ') letter_no,
+                        SUM(sus.amount) uc_submit
+                      FROM soe_uc_submit sus
+                        LEFT JOIN soe_uc_allotment sua
+                          ON sus.allotment_id = sua.id
+                      WHERE sua.deleted_at IS NULL
+                        AND sus.deleted_at IS NULL
+                      GROUP BY sua.year) sbmt
+              ON allt.year = sbmt.year
+              LEFT JOIN soe_years y ON y.id = allt.year";
 
-        return $this->db->query($sql)->getResult();
-	}
+        return $this->db->query($sql, [$recipient_id])->getResult();
+    }
 
-    public function getSubmissions($filter=[]) {
+    // Get submissions based on filter
+    public function getSubmissions(array $filter = []): array
+    {
         $sql = "SELECT
-  sus.id uc_id,
-  sus.date_submit,
-  sus.letter_no,
-  sus.page_no,
-  sus.amount,
-  sus.document
-FROM soe_uc_submit sus
-  LEFT JOIN soe_uc_allotment sua
-    ON sus.allotment_id = sua.id
-WHERE sua.deleted_at IS NULL
-AND sus.deleted_at IS NULL
-AND sua.year = ".$filter['year']."
-AND sua.recipient_id = ".$filter['recipient_id'];
+            sus.id uc_id,
+            sus.date_submit,
+            sus.letter_no,
+            sus.page_no,
+            sus.amount,
+            sus.document
+          FROM soe_uc_submit sus
+            LEFT JOIN soe_uc_allotment sua
+              ON sus.allotment_id = sua.id
+          WHERE sua.deleted_at IS NULL
+            AND sus.deleted_at IS NULL
+            AND sua.year = ?
+            AND sua.recipient_id = ?";
 
-        return $this->db->query($sql)->getResult();
-	}
+        return $this->db->query($sql, [$filter['year'], $filter['recipient_id']])->getResult();
+    }
 }
