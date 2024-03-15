0d5a241af01ee9e024787c024fca9c79ef8d49bf
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:08 +0530

    improve

--- admin/Reports/Controllers/Mpr.php
+++ admin/Reports/Controllers/Mpr.php
@@ -1,6 +1,20 @@
-<?php 
+<?php
+
 namespace Admin\Reports\Controllers;
 
 use App\Controllers\AdminController;
 use App\Libraries\Export;
-use App\Traits
+use App\Traits\SomeTrait;
+
+class ReportController extends AdminController
+{
+    use SomeTrait;
+
+    public function exportReport()
+    {
+        // Your code here
+
+        $export = new Export();
+        $export->exportData($data);
+    }
+}
