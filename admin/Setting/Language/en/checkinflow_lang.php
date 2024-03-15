251450a42d1bb812d554a2f51726d7fecd7246a7
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:11 +0530

    improve

--- admin/Setting/Language/en/checkinflow_lang.php
+++ admin/Setting/Language/en/checkinflow_lang.php
@@ -1,4 +1,6 @@
 <?php
+// Language file for settings page
+
 // Heading
 $lang['heading_title']                    = 'Checkin Flow';
 
@@ -8,128 +10,100 @@ $lang['text_image']                       = 'Select Image';
 $lang['text_clear']                       = 'Clear';
 $lang['text_success']                     = 'Success: You have modified settings!';
 
-// Entry
-$lang['entry_title']                      = 'Site Title';
-$lang['entry_tagline']                    = 'Site Tagline';
-$lang['entry_logo']                    	= 'Site Logo';
-$lang['entry_icon']                       = 'Site Icon';
-$lang['entry_meta_title']                 = 'Meta Title';
-$lang['entry_meta_description']           = 'Meta Tag Description';
-$lang['entry_meta_keyword']               = 'Meta Tag Keywords';
+// Entries for general settings
+$lang['entry_general'] = [
+    'site_title'                      => 'Site Title',
+    'site_tagline'                    => 'Site Tagline',
+    'site_logo'                    	=> 'Site Logo',
+    'site_icon'                       => 'Site Icon',
+    'meta_title'                 => 'Meta Title',
+    'meta_description'           => 'Meta Tag Description',
+    'meta_keyword'               => 'Meta Tag Keywords',
+    'site_owner'                 => 'Site Owner',
+    'address'                 	=> 'Address',
+    'country'                    => 'Country',
+    'state'                      => 'Region / State',
+    'email'                      => 'E-Mail',
+    'telephone'                  => 'Telephone',
+    'fax'                        => 'Fax',
+];
 
-$lang['entry_site_owner']                 = 'Site Owner';
-$lang['entry_address']                 	= 'Address';
-$lang['entry_country']                    = 'Country';
-$lang['entry_state']                      = 'Region / State';
-$lang['entry_email']                      = 'E-Mail';
-$lang['entry_telephone']                  = 'Telephone';
-$lang['entry_fax']                        = 'Fax';
+// Entries for library and issue settings
+$lang['entry_library'] = [
+    'library_fine'               => 'Fine',
+    'issue_limit_books'          => 'Issue Limit - Books ',
+    'issue_limit_days'           => 'Issue Limit - Days ',
+    'auto_fine'                  => 'Automatic Fine',
+    'receipt_prefix'             => 'Receipt Prefix',
+    'display_stock'              => 'Book Stock Display',
+    'stock_warning'              => 'Book Stock Warning',
+    'mail_alert'              	=> 'Mail Alert',
+    'sms_alert'              		=> 'SMS Alert',
+    'delay_members'             	=> 'Delay members Warning',
+];
 
-$lang['entry_library_fine']               = 'Fine';
-$lang['entry_issue_limit_books']          = 'Issue Limit - Books ';
-$lang['entry_issue_limit_days']           = 'Issue Limit - Days ';
-$lang['entry_auto_fine']                  = 'Automatic Fine';
-$lang['entry_receipt_prefix']             = 'Receipt Prefix';
-$lang['entry_display_stock']              = 'Book Stock Display';
-$lang['entry_stock_warning']              = 'Book Stock Warning';
-$lang['entry_mail_alert']              	= 'Mail Alert';
-$lang['entry_sms_alert']              		= 'SMS Alert';
-$lang['entry_delay_members']             	= 'Delay members Warning';
+// Entries for design settings
+$lang['entry_design'] = [
+    'site_homepage'              => 'Site Homepage',
+    'front_theme'                => 'Front Theme',
+    'front_template'             => 'Front Default Layout',
+    'header_layout'              => 'Header Layout',
+    'header_image'               => 'Header Image',
+    'header_banner'              => 'Header Banner',
+    'header_slider'              => 'Header Slider',
+    'background_image'           => 'Background Image',
+    'background_position'        => 'Background Position',
+    'background_repeat'          => 'Background Repeat',
+    'background_attachment'      => 'Background Attachment',
+    'background_color'      	  => 'Background Color',
+    'text_color'      			  => 'Text Color',
+];
 
-$lang['entry_site_homepage']              = 'Site Homepage';
-$lang['entry_front_theme']                = 'Front Theme';
-$lang['entry_front_template']             = 'Front Default Layout';
-$lang['entry_header_layout']              = 'Header Layout';
-$lang['entry_header_image']               = 'Header Image';
-$lang['entry_header_banner']              = 'Header Banner';
-$lang['entry_header_slider']              = 'Header Slider';
-$lang['entry_background_image']           = 'Background Image';
-$lang['entry_background_position']        = 'Background Position';
-$lang['entry_background_repeat']          = 'Background Repeat';
-$lang['entry_background_attachment']      = 'Background Attachment';
-$lang['entry_background_color']      	  = 'Background Color';
-$lang['entry_text_color']      			  = 'Text Color';
+// Entries for FTP and mail settings
+$lang['entry_ftp'] = [
+    'ftp_host'      			  => 'FTP Host',
+    'ftp_port'      			  => 'FTP Port',
+    'ftp_username'      		  => 'FTP Username',
+    'ftp_password'      		  => 'FTP Password',
+    'ftp_root'      			  => 'FTP Root',
+    'ftp_enable'      			  => 'FTP Enable',
+];
 
-$lang['entry_ftp_host']      			  = 'FTP Host';
-$lang['entry_ftp_port']      			  = 'FTP Port';
-$lang['entry_ftp_username']      		  = 'FTP Username';
-$lang['entry_ftp_password']      		  = 'FTP Password';
-$lang['entry_ftp_root']      			  = 'FTP Root';
-$lang['entry_ftp_enable']      			  = 'FTP Enable';
+$lang['entry_mail'] = [
+    'mail_protocol'      		  => 'Mail Protocol',
+    'mail_parameter'      		  => 'Mail Parameters',
+    'smtp_host'      		      => 'SMTP Hostname',
+    'smtp_username'      		  => 'SMTP Username',
+    'smtp_password'      		  => 'SMTP Password',
+    'smtp_port'      			  => 'SMTP Port',
+    'smtp_timeout'      		  => 'SMTP Timeout',
+];
 
-$lang['entry_mail_protocol']      		  = 'Mail Protocol';
-$lang['entry_mail_parameter']      		  = 'Mail Parameters';
-$lang['entry_smtp_host']      		      = 'SMTP Hostname';
-$lang['entry_smtp_username']      		  = 'SMTP Username';
-$lang['entry_smtp_password']      		  = 'SMTP Password';
-$lang['entry_smtp_port']      			  = 'SMTP Port';
-$lang['entry_smtp_timeout']      		  = 'SMTP Timeout';
+// Entries for miscellaneous settings
+$lang['entry_misc'] = [
+    'ssl'                        => 'Use SSL',
+    'robots'                     => 'Robots',
+    'time_zone'                  => 'Time Zone',
+    'date_format'                => 'Date Format',
+    'time_format'                => 'Time Format',
+    'pagination_limit_front'     => 'Pagination Limit (Front)',
+    'pagination_limit_admin'     => 'Pagination Limit (Admin)',
+    'seo_url'                    => 'Use SEO URLs',
+    'file_max_size'	          => 'Max File Size',
+    'file_extensions'            => 'Allowed File Extensions',
+    'file_mimetypes'             => 'Allowed File Mime Types',
+    'maintenance_mode'           => 'Maintenance Mode',
+    'encryption_key'             => 'Encryption Key',
+    'compression_level'          => 'Output Compression Level',
+    'display_error'              => 'Display Errors',
+    'log_error'                  => 'Log Errors',
+    'error_log_filename'         => 'Error Log Filename',
+    'status'                     => 'Status',
+];
 
-$lang['entry_ssl']                        = 'Use SSL';
-$lang['entry_robots']                     = 'Robots';
-$lang['entry_time_zone']                  = 'Time Zone';
-$lang['entry_date_format']                = 'Date Format';
-$lang['entry_time_format']                = 'Time Format';
-$lang['entry_pagination_limit_front']     = 'Pagination Limit (Front)';
-$lang['entry_pagination_limit_admin']     = 'Pagination Limit (Admin)';
-
-
-$lang['entry_seo_url']                    = 'Use SEO URLs';
-$lang['entry_file_max_size']	          = 'Max File Size';
-$lang['entry_file_extensions']            = 'Allowed File Extensions';
-$lang['entry_file_mimetypes']             = 'Allowed File Mime Types';
-$lang['entry_maintenance_mode']           = 'Maintenance Mode';
-$lang['entry_encryption_key']             = 'Encryption Key';
-$lang['entry_compression_level']          = 'Output Compression Level';
-$lang['entry_display_error']              = 'Display Errors';
-$lang['entry_log_error']                  = 'Log Errors';
-$lang['entry_error_log_filename']         = 'Error Log Filename';
-$lang['entry_status']                     = 'Status';
-
-// Help
-$lang['help_ssl']                         = 'To use SSL check with your host if a SSL certificate is installed and add the SSL URL to the catalog and admin config files.';
-$lang['help_robots']                      = 'A list of web crawler user agents that shared sessions will not be used with. Use separate lines for each user agent.';
-$lang['help_seo_url']                     = 'To use SEO URLs, apache module mod-rewrite must be installed and you need to rename the htaccess.txt to .htaccess.';
-$lang['help_file_max_size']		          = 'The maximum image file size you can upload in Image Manager. Enter as byte.';
-$lang['help_file_ext_allowed']            = 'Add which file extensions are allowed to be uploaded. Use a new line for each value.';
-$lang['help_file_mime_allowed']           = 'Add which file mime types are allowed to be uploaded. Use a new line for each value.';
-$lang['help_maintenance']                 = 'Prevents customers from browsing your store. They will instead see a maintenance message. If logged in as admin, you will see the store as normal.';
-$lang['help_password']                    = 'Allow forgotten password to be used for the admin. This will be disabled automatically if the system detects a hack attempt.';
-$lang['help_encryption']                  = 'Please provide a secret key that will be used to encrypt private information when processing orders.';
-$lang['help_compression']                 = 'GZIP for more efficient transfer to requesting clients. Compression level must be between 0 - 9.';
-$lang['help_google_analytics']            = 'Login to your <a href="http://www.google.com/analytics/" target="_blank"><u>Google Analytics</u></a> account and after creating your website profile copy and paste the analytics code into this field.';
-$lang['help_google_captcha']              = 'Go to <a href="https://www.google.com/recaptcha/intro/index.html" target="_blank"><u>Google reCAPTCHA page</u></a> and register your website.';
-
-
-// Error
-$lang['error_warning']                    = 'Warning: Please check the form carefully for errors!';
-$lang['error_permission']                 = 'Warning: You do not have permission to modify settings!';
-$lang['error_name']                       = 'Store Name must be between 3 and 32 characters!';
-$lang['error_owner']                      = 'Store Owner must be between 3 and 64 characters!';
-$lang['error_address']                    = 'Store Address must be between 10 and 256 characters!';
-$lang['error_email']                      = 'E-Mail Address does not appear to be valid!';
-$lang['error_telephone']                  = 'Telephone must be between 3 and 32 characters!';
-$lang['error_meta_title']                 = 'Title must be between 3 and 32 characters!';
-$lang['error_limit']       	           = 'Limit required!';
-$lang['error_login_attempts']       	   = 'Login Attempts must be greater than 0!';
-$lang['error_customer_group_display']     = 'You must include the default customer group if you are going to use this feature!';
-$lang['error_voucher_min']                = 'Minimum voucher amount required!';
-$lang['error_voucher_max']                = 'Maximum voucher amount required!';
-$lang['error_processing_status']          = 'You must choose at least 1 order process status';
-$lang['error_complete_status']            = 'You must choose at least 1 order complete status';
-$lang['error_image_thumb']                = 'Product Image Thumb Size dimensions required!';
-$lang['error_image_popup']                = 'Product Image Popup Size dimensions required!';
-$lang['error_image_product']              = 'Product List Size dimensions required!';
-$lang['error_image_category']             = 'Category List Size dimensions required!';
-$lang['error_image_additional']           = 'Additional Product Image Size dimensions required!';
-$lang['error_image_related']              = 'Related Product Image Size dimensions required!';
-$lang['error_image_compare']              = 'Compare Image Size dimensions required!';
-$lang['error_image_wishlist']             = 'Wish List Image Size dimensions required!';
-$lang['error_image_cart']                 = 'Cart Image Size dimensions required!';
-$lang['error_image_location']             = 'Store Image Size dimensions required!';
-$lang['error_ftp_hostname']               = 'FTP Host required!';
-$lang['error_ftp_port']                   = 'FTP Port required!';
-$lang['error_ftp_username']               = 'FTP Username required!';
-$lang['error_ftp_password']               = 'FTP Password required!';
-$lang['error_error_filename']             = 'Error Log Filename required!';
-$lang['error_encryption']                 = 'Encryption Key must be between 3 and 32 characters!';
+// Helps for various settings
+$lang['help'] = [
+    'ssl'                         => 'To use SSL, check with your host if a SSL certificate is installed and add the SSL URL to the catalog and admin config files.',
+    'robots'                      => 'A list of web crawler user agents that shared sessions will not be used with. Use separate lines for each user agent.',
+    'seo_url'                     => 'To use SEO URLs, apache module mod-rewrite must be installed and you need to rename the htaccess.txt to .htaccess.',
+    'file_max_size'		          => 'The maximum image file size you can upload in
