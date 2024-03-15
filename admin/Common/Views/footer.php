2cc12d9b9a010ada6f33f2b8006d06d214c61f20
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:00 +0530

    improve

--- admin/Common/Views/footer.php
+++ admin/Common/Views/footer.php
@@ -2,14 +2,17 @@
 $template = service('template');
 $user = service('user');
 $showOldPortal = $variables['show_old_portal'] ?? false; // Added to avoid undefined variable notice
+$headSection = view('partials/head'); // Assuming there is a partial view for the head section
+$content = view('partials/content'); // Assuming there is a partial view for the content
+$footerScripts = view('partials/footer-scripts'); // Assuming there is a partial view for the footer scripts
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <!-- Add missing head section here -->
+  <?= $headSection; ?>
 </head>
 <body class="bg-image" style="background-image: url('<?=theme_url('assets/media/photos/photo34@2x.jpg');?>');">
-  <!-- Add missing content here -->
+  <?= $content; ?>
 
   <footer id="page-footer" class="opacity-0">
     <div class="content py-20 font-size-sm clearfix">
@@ -20,10 +23,11 @@ $showOldPortal = $variables['show_old_portal'] ?? false; // Added to avoid undef
   </footer>
 </div>
 
+<?= $template->footer_javascript() ?>
+
 <script src="<?=theme_url('assets/js/codebase.app.js');?>"></script>
 <script src="<?=theme_url('assets/js/common.js');?>"></script>
 <script src="//cdn.jsdelivr.net/npm/gasparesganga-jquery-loading-overlay@2.1.7/dist/loadingoverlay.min.js"></script>
-<?php echo $template->footer_javascript() ?>
 
 <script>
   $(function () {
@@ -31,7 +35,4 @@ $showOldPortal = $variables['show_old_portal'] ?? false; // Added to avoid undef
       $('.old-portal-login').hide();
     }
   });
-</script>
 
-</body>
-</html>
