0188dc822f0fa8d394e2be0247013db66be9684f
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:01 +0530

    improve

--- app/Config/Encryption.php
+++ app/Config/Encryption.php
@@ -5,63 +5,52 @@ namespace Config;
 use CodeIgniter\Config\BaseConfig;
 
 /**
- * Encryption configuration.
+ * Encryption configuration class.
  *
  * These are the settings used for encryption, if you don't pass a parameter
  * array to the encrypter for creation/initialization.
  */
 class Encryption extends BaseConfig
 {
-	/**
-	 * --------------------------------------------------------------------------
-	 * Encryption Key Starter
-	 * --------------------------------------------------------------------------
-	 *
-	 * If you use the Encryption class you must set an encryption key (seed).
-	 * You need to ensure it is long enough for the cipher and mode you plan to use.
-	 * See the user guide for more info.
-	 *
-	 * @var string
-	 */
-	public $key = '';
+    /**
+     * Encryption key (seed)
+     *
+     * If you use the Encryption class, you must set an encryption key (seed).
+     * You need to ensure it is long enough for the cipher and mode you plan to use.
+     *
+     * @var string
+     */
+    public $encryptionKey = '';
 
-	/**
-	 * --------------------------------------------------------------------------
-	 * Encryption Driver to Use
-	 * --------------------------------------------------------------------------
-	 *
-	 * One of the supported encryption drivers.
-	 *
-	 * Available drivers:
-	 * - OpenSSL
-	 * - Sodium
-	 *
-	 * @var string
-	 */
-	public $driver = 'OpenSSL';
+    /**
+     * Encryption driver to use
+     *
+     * One of the supported encryption drivers.
+     *
+     * Available drivers:
+     * - OpenSSL
+     * - Sodium
+     *
+     * @var string
+     */
+    public $driver = 'OpenSSL';
 
-	/**
-	 * --------------------------------------------------------------------------
-	 * SodiumHandler's Padding Length in Bytes
-	 * --------------------------------------------------------------------------
-	 *
-	 * This is the number of bytes that will be padded to the plaintext message
-	 * before it is encrypted. This value should be greater than zero.
-	 *
-	 * See the user guide for more information on padding.
-	 *
-	 * @var integer
-	 */
-	public $blockSize = 16;
+    /**
+     * SodiumHandler's padding length in bytes
+     *
+     * This is the number of bytes that will be padded to the plaintext message
+     * before it is encrypted. This value should be greater than zero.
+     *
+     * @var integer
+     */
+    public $paddingLength = 16;
 
-	/**
-	 * --------------------------------------------------------------------------
-	 * Encryption digest
-	 * --------------------------------------------------------------------------
-	 *
-	 * HMAC digest to use, e.g. 'SHA512' or 'SHA256'. Default value is 'SHA512'.
-	 *
-	 * @var string
-	 */
-	public $digest = 'SHA512';
+    /**
+     * Encryption digest
+     *
+     * HMAC digest to use, e.g. 'SHA512' or 'SHA256'. Default value is 'SHA512'.
+     *
+     * @var string
+     */
+    public $hmacDigest = 'SHA512';
 }
