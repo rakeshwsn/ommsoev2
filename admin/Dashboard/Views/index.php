d7c82e3986cfc4eeb116cb854ee8e39ba59e323e
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:54 +0530

    improve

--- admin/Dashboard/Views/index.php
+++ admin/Dashboard/Views/index.php
@@ -1,3 +1,20 @@
 <section>
-    <h1>Module Customer => Index</h1>
+  <h1>Customer Module</h1>
+  <p>Welcome to the Customer Module! This section of the application is dedicated to managing customer information and interactions. Here, you can view, add, edit, and delete customer records, as well as perform various other customer-related tasks.</p>
+  <h2>Customer List</h2>
+  <p>Here is a list of all the customers in our system:</p>
+  <ul>
+    <li>Customer 1</li>
+    <li>Customer 2</li>
+    <li>Customer 3</li>
+    <!-- Add more customers here as needed -->
+  </ul>
+  <h2>Customer Management Tools</h2>
+  <p>Use the following tools to manage your customers:</p>
+  <ul>
+    <li><a href="/customer/add">Add New Customer</a></li>
+    <li><a href="/customer/edit">Edit Customer Information</a></li>
+    <li><a href="/customer/delete">Delete Customer</a></li>
+    <!-- Add more management tools here as needed -->
+  </ul>
 </section>
