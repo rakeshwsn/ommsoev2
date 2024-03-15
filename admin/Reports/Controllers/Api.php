d6ace28b79af35b3171d8c4e7457f3edb5aa961e
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:25 +0530

    improve

--- admin/Reports/Controllers/Api.php
+++ admin/Reports/Controllers/Api.php
@@ -1,77 +1,81 @@
-<?php 
+<?php
+
 namespace Admin\Reports\Controllers;
+
 use Admin\Common\Models\YearModel;
 use Admin\Localisation\Models\BlockModel;
 use Admin\Localisation\Models\DistrictModel;
 use Admin\Reports\Models\AreaModel;
 use Admin\Reports\Models\ReportsModel;
-use App\Traits\PreloaderTrait;
-use App\Traits\ReportTrait;
-use App\Traits\TreeTrait;
 use CodeIgniter\API\ResponseTrait;
-use CodeIgniter\RESTful\ResourceController;
 
-class Api extends ResourceController
+class Api extends \CodeIgniter\RESTful\ResourceController
 {
-    use ResponseTrait,TreeTrait,ReportTrait {
-        ReportTrait::getTable insteadof TreeTrait;
-        ReportTrait::generateTable insteadof TreeTrait;
+    use ResponseTrait;
+
+    public function __construct()
+    {
+        helper(['aio', 'date', 'general', 'number']);
     }
 
-    function __construct() {
-        helper(['aio','date','general','number']);
-    }
+    public function mpr()
+    {
+        $rules = [
+            'year' => 'permit_optional|is_natural_no_zero',
+            'month' => 'permit_optional|is_natural_no_zero',
+            'agency_type_id' => 'permit_optional|is_natural_no_zero',
+            'fund_agency_id' => 'permit_optional|is_natural_no_zero',
+            'district_id' => 'permit_optional|is_natural_no_zero',
+            'block_id' => 'permit_optional|is_natural_no_zero',
+        ];
 
-    public function mpr() {
+        if (!$this->validate($rules)) {
+            return $this->fail($this->validator->getErrors());
+        }
 
-        $data = [];
+        $filter = [
+            'year_id' => getCurrentYearId(),
+            'month_id' => getCurrentMonthId(),
+            'agency_type_id' => '',
+            'fund_agency_id' => 1,
+            'block_users' => [5, 6],
+            'block_user' => false,
+        ];
 
-        $filter['year_id'] = getCurrentYearId();
-        if($this->request->getGet('year')){
+        if ($this->request->getGet('year')) {
             $filter['year_id'] = $this->request->getGet('year');
         }
 
-        $filter['month_id'] = getCurrentMonthId();
-        if($this->request->getGet('month')){
+        if ($this->request->getGet('month')) {
             $filter['month_id'] = $this->request->getGet('month');
         }
 
-        $filter['agency_type_id'] = '';
-//        if($this->request->getGet('agency_type_id')){
-//            $filter['agency_type_id'] = $this->request->getGet('agency_type_id');
-//        }
+        if ($this->request->getGet('agency_type_id')) {
+            $filter['agency_type_id'] = $this->request->getGet('agency_type_id');
+        }
 
-        $filter['fund_agency_id'] = 1;
-        $filter['block_users'] = [5,6];
-        $filter['block_user'] = false;
-
-        if($this->request->getGet('fund_agency_id')){
+        if ($this->request->getGet('fund_agency_id')) {
             $filter['fund_agency_id'] = $this->request->getGet('fund_agency_id');
         }
 
-        $filter['district_id'] = '';
-        if($this->request->getGet('district_id')){
+        if ($this->request->getGet('district_id')) {
             $filter['district_id'] = $this->request->getGet('district_id');
         }
 
-        $filter['block_id'] = '';
-        if($this->request->getGet('block_id')){
+        if ($this->request->getGet('block_id')) {
             $filter['block_id'] = $this->request->getGet('block_id');
         }
 
         $reportModel = new ReportsModel();
-        $data['components'] = [];
-
         $components = $reportModel->getMpr($filter);
 
         $components = $this->buildTree($components, 'parent', 'scomponent_id');
 
-        $data['components'] = $components;
-
-        return $this->respond($data);
+        return $this->respond($components);
     }
 
-    public function mprFilters() {
+    public function mprFilters()
+    {
         $data['districts'] = (new DistrictModel())->getDistrictsByFundAgency(1);
         $data['months'] = getAllMonths();
         $data['years'] = getAllYears();
@@ -80,39 +84,41 @@ class Api extends ResourceController
         return $this->respond($data);
     }
 
-    public function getBlocks() {
-        $district_id = 0;
-        if($this->request->getGet('district_id')){
-            $district_id = $this->request->getGet('district_id');
-        }
+    public function getBlocks()
+    {
+        $district_id = $this->request->getGet('district_id');
         $data['blocks'] = (new BlockModel())->getBlocksByDistrict($district_id);
 
         return $this->respond($data);
     }
 
-    public function getAreaCoverage() {
+    public function getAreaCoverage()
+    {
         $areaModel = new AreaModel();
         $yearModel = new YearModel();
 
-        $filter['year'] = $areaModel->getYearByDate(date('Y-m-d'))->id;
-        if($this->request->getGet('year')){
+        $filter = [
+            'year' => $areaModel->getYearByDate(date('Y-m-d'))->id,
+            'season' => $areaModel->getCurrentSeason(),
+            'district_id' => '',
+            'block_id' => '',
+        ];
+
+        if ($this->request->getGet('year')) {
             $year = $this->request->getGet('year');
             $year_sdate = $yearModel->find($year)->start_date;
             $filter['year'] = $areaModel->getYearByDate($year_sdate)->id;
         }
 
-        $filter['season'] = $areaModel->getCurrentSeason();
-        if($this->request->getGet('season')){
+        if ($this->request->getGet('season')) {
             $filter['season'] = $this->request->getGet('season');
         }
 
-        $filter['district_id'] = '';
-        if($this->request->getGet('district_id')){
+        if ($this->request->getGet('district_id')) {
             $filter['district_id'] = $this->request->getGet('district_id');
         }
 
-        $filter['block_id'] = '';
-        if($this->request->getGet('block_id')){
+        if ($this->request->getGet('block_id')) {
             $filter['block_id'] = $this->request->getGet('block_id');
         }
 
