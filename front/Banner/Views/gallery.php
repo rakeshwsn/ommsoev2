f607e45deb9843166be6ea821bbef1aa29bae058
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:10 +0530

    improve

--- front/Banner/Views/gallery.php
+++ front/Banner/Views/gallery.php
@@ -3,16 +3,20 @@
 		<h2>Gallery</h2>
 		<h3>Our Works</h3>
 	</div>
-	<?php if($sliders){?>
+	<?php if (is_array($sliders)) : ?>
 		<div class="grid grid-four-col kc_image_gallery" id="kehl-grid">
 			<div class="grid-sizer">&nbsp;</div>
-			<?php foreach($sliders as $slider){?>
-			<div class="grid-box fruits">
-				<a class="image-popup-vertical-fit kc-image-link" href="<?=$slider['image']?>">
-					<img alt="" src="<?=$slider['image']?>" /> 
-				</a>
-			</div>
-			<?}?>
+			<?php foreach ($sliders as $slider) : ?>
+				<?php if (isset($slider['image'])) : ?>
+					<div class="grid-box fruits">
+						<a class="image-popup-vertical-fit kc-image-link" href="<?= $slider['image'] ?>">
+							<img alt="Slider Image" src="<?= $slider['image'] ?>" height="200" width="300" /> 
+						</a>
+					</div>
+				<?php endif; ?>
+			<?php endforeach; ?>
 		</div>
-	<?}?>
+	<?php else : ?>
+		<p>No sliders found.</p>
+	<?php endif; ?>
 </div>
