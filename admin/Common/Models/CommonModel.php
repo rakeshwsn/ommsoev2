78cef924ed1a4b47c2c8dd0933963be4939f64b4
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:58 +0530

    improve

--- admin/Common/Models/CommonModel.php
+++ admin/Common/Models/CommonModel.php
@@ -3,4 +3,28 @@
 namespace Admin\Common\Models;
 
 use CodeIgniter\Model;
-use CodeIgniter\Database\Exception;
+use CodeIgniter\Database\Exception\DatabaseException;
+
+class BaseModel extends Model
+{
+    protected $db;
+
+    public function __construct()
+    {
+        parent::__construct();
+        try {
+            $this->db = db_connect();
+        } catch (DatabaseException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    protected function runQuery($sql, $data = [])
+    {
+        try {
+            return $this->db->query($sql, $data);
+        } catch (DatabaseException $e) {
+            exit($e->getMessage());
+        }
+    }
+}
