17bd54c04abc171ec0d8d7dcedb2a34f5fd37523
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:43:53 +0530

    improve

--- admin/Enterprises/Config/Routes.php
+++ admin/Enterprises/Config/Routes.php
@@ -6,15 +6,25 @@ if (!isset($routes)) {
     $routes = \Config\Services::routes(true);
 }
 
-$routes->group('admin', ['namespace' => 'Admin', 'filter' => 'login'], function ($routes) {
-    $routes->add('enterprises', 'Enterprises\Controllers\Enterprises::index');
-    $routes->post('enterprises/search', 'Enterprises\Controllers\Enterprises::search');
-    // $routes->post('incentive/searchmain', 'Incentive\Controllers\Incentive::searchMain');
+// Group all admin enterprise routes
+$routes->group('admin', [
+    'namespace' => 'Admin',
+    'filter' => 'login'
+], function ($routes) {
+    // The index page for enterprises
+    $routes->add('enterprises', 'Enterprises\Controllers\Enterprise::index');
+
+    // Search for enterprises
+    $routes->post('enterprises/search', 'Enterprises\Controllers\Enterprise::search');
+
+    // Additional routes for incentives have been commented out
+    // Uncomment these lines if you want to add them back
+
     // $routes->match(['get', 'post'], 'incentive/view/(:segment)', 'Incentive\Controllers\Incentive::view/$1');
     // $routes->match(['get', 'post'], 'incentive/add', 'Incentive\Controllers\Incentive::add');
     // $routes->match(['get', 'post'], 'incentive/addform', 'Incentive\Controllers\Incentive::addform');
     // $routes->match(['get', 'post'], 'incentive/edit/(:segment)', 'Incentive\Controllers\Incentive::edit/$1');
-    // $routes->get('incentivemain/delete/(:segment)',   'Incentive\Controllers\Incentive::delete/$1');
+    // $routes->get('incentivemain/delete/(:segment)', 'Incentive\Controllers\Incentive::delete/$1');
     // $routes->post('incentive/delete', 'Incentive\Controllers\Incentive::delete');
     // $routes->add('incentive/incentivesearch', 'Incentive\Controllers\Incentive::incentivesearch');
     // $routes->post('incentive/searchall', 'Incentive\Controllers\Incentive::searchall');
