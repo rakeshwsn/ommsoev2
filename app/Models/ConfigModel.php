fdec1e87fd00ef846bafa974a815d99705f58448
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:45:23 +0530

    improve

--- app/Models/ConfigModel.php
+++ app/Models/ConfigModel.php
@@ -6,29 +6,29 @@ use CodeIgniter\Model;
 
 class ConfigModel extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'config';
-    protected $primaryKey       = 'id';
+    protected $DBGroup = 'default';
+    protected $table = 'config';
+    protected $primaryKey = 'id';
     protected $useAutoIncrement = true;
-    protected $insertID         = 0;
-    protected $returnType       = 'object';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = ['key', 'value'];
+    protected $insertID = 0;
+    protected $returnType = 'object';
+    protected $useSoftDeletes = false;
+    protected $protectFields = true;
+    protected $allowedFields = ['key', 'value'];
 
     // Dates
     protected $useTimestamps = true;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
 
     // Validation
-    protected $validationRules      = [
+    protected $validationRules = [
         'key' => 'required|alpha_dash|min_length[3]|max_length[50]',
         'value' => 'required'
     ];
-    protected $validationMessages   = [
+    protected $validationMessages = [
         'key' => [
             'required' => 'The configuration key is required.',
             'alpha_dash' => 'The configuration key may only contain letters, numbers, and dashes.',
@@ -39,18 +39,17 @@ class ConfigModel extends Model
             'required' => 'The configuration value is required.'
         ]
     ];
-    protected $skipValidation       = false;
+    protected $skipValidation = false;
     protected $cleanValidationRules = true;
 
     // Callbacks
     protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
 }
-
