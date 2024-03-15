1ee7bd994e52e09e303fd9f7649e0fc9c90ed10e
Rakesh <59045085+rakeshwsn@users.noreply.github.com>
2024-03-15 12:44:57 +0530

    improve

--- admin/Common/Language/en/Banner.php
+++ admin/Common/Language/en/Banner.php
@@ -1,23 +1,12 @@
 <?php
 
-// Define the array of text values
-$text = [
+// Define the array of text values as a constant
+define('TEXT', [
     'heading_title'        => 'Banner',
     'text_image'           => 'Select Image',
     'text_clear'            => 'Clear'
-];
-
-// Return the array of text values
-function getText() {
-    global $text;
-    return $text;
-}
-
-// Call the function to get the text values
-$textValues = getText();
+]);
 
 // Print the text values
-echo $textValues['heading_title'];
-echo $textValues['text_image'];
-echo $textValues['text_clear'];
+echo TEXT['heading_title'], TEXT['text_image'], TEXT['text_clear'];
 
